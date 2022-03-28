82f81cfa0b114a985d9a9529ca635073046e4b00
Torrey Smith <tas.smity@yahoo.com>
2022-03-27 19:47:57 -0700

    adding PrismaAdapter into the nextauth api call

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,3 +1,5 @@
+
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from '../../../lib/prisma'
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
