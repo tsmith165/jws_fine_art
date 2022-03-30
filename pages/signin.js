be08e7580bf3e96bb73d2dd5d24a4db2be5acd13
Torrey Smith <tas.smity@yahoo.com>
2022-03-30 08:34:35 -0700

    first implementation of next_auth_generic

--- pages/signin.js
+++ pages/signin.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { signIn } from "next-auth/react";
 import { useSession } from '../lib/next-auth-react-query';
 
