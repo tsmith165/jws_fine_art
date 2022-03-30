7794facc604c9e6716d64fd2f46a0c45587bbfc0
Torrey Smith <tas.smity@yahoo.com>
2022-03-30 09:44:49 -0700

    fixing prisma client import

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 
-
-// add prisma to the NodeJS global type
-
-interface CustomNodeJsGlobal extends Global {
-  prisma: PrismaClient
+declare global {
+    var prisma: PrismaClient | undefined
 }
 
-// Prevent multiple instances of Prisma Client in development
+export const prisma = global.prisma || new PrismaClient({log: ['query'],})
 
-declare const global: CustomNodeJsGlobal
-
-export const prisma = global.prisma || new PrismaClient()
-
-global.prisma = prisma
-
-export default prisma
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
