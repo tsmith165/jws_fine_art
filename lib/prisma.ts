c7655287787a7e03989ca466be96c2556e8ab418
Torrey Smith <tas.smity@yahoo.com>
2022-03-30 09:56:31 -0700

    fixing prisma client import

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,6 +4,8 @@ declare global {
     var prisma: PrismaClient | undefined
 }
 
+console.log(`Current Global Prisma: ${global.prisma}`)
+
 export const prisma = global.prisma || new PrismaClient({log: ['query'],})
 
 //if (process.env.NODE_ENV !== 'production') global.prisma = prisma
