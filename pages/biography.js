8b404a1e67ab9fdc19732b928673b48526c2887a
Torrey Smith <tas.smity@yahoo.com>
2022-04-06 21:55:51 -0700

    added bio

---
+++ pages/biography.js
@@ -0,0 +1,10 @@
+import PageLayout from '../src/components/layout/PageLayout'
+import BiographyLayout from '../src/components/BiographyLayout';
+
+export default function Biography({}) {
+  return (
+    <PageLayout>
+      <BiographyLayout/>
+    </PageLayout>
+  )
+}
--- src/components/layout/PageLayout.js
+++ src/components/layout/PageLayout.js
@@ -8,6 +8,9 @@ const PageLayout = (props) => {
                 <title>JWS Fine Art</title>
                 <meta name="description" content="JWS Fine Art" />
                 <link rel="icon" href="/JWS_ICON.png" />
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                <link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:wght@300&family=Lato:wght@300&display=swap" rel="stylesheet"/>
+
             </Head>
             <main className={styles.main}>
                 {props.children}
