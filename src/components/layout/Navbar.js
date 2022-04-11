c38007c7a606fd448589552654594d46be20aeff
Torrey Smith <tas.smity@yahoo.com>
2022-04-10 20:50:25 -0700

    setting up customer hover/click for menu overlay

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,10 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import React, { useState } from 'react';
+
 import Menu from './Menu'
 import Profile from './Profile'
+
 import styles from "../../../styles/Navbar.module.scss"
 
 import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
@@ -9,6 +12,21 @@ import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 const Navbar = ({}) => {
     const use_account_icon_as_link = true;
+
+    const [menu_open, set_menu_open] = useState(false)
+
+    function menu_clicked(event) {
+        event.preventDefault()
+        console.log(`Setting Menu To: ${menu_open ? false : true}`)
+        set_menu_open(menu_open ? false : true)
+    }
+
+    function menu_hovered(event, mouse_in) {
+        event.preventDefault()
+        console.log(`Setting Menu To: ${mouse_in}`)
+        set_menu_open(mouse_in)
+    }
+
 
     return (
         <nav className={styles.navbar}>
@@ -51,16 +69,23 @@ const Navbar = ({}) => {
                     </div>
                 </div>
                 
-                <div className={styles.page_menu_full_container}>
-                    <div className={styles.menu_button_container}>
+                <div className={styles.page_menu_full_container} onMouseOver={ (e) => { menu_hovered(e, true) }} onMouseLeave={ (e) => { menu_hovered(e, false) }}>
+                    <div className={styles.menu_button_container} >
                         <MenuRoundedIcon className={styles.hamburger_button} />
                     </div>
+                    
+                    {
+                        menu_open == true ? (
+                            <div className={styles.page_menu_container}>
+                                <div className={styles.page_menu_body}>
+                                    <Menu set_menu_open={set_menu_open}/>
+                                </div>
+                            </div>
+                        ) : (
+                            null
+                        )
+                    }
 
-                    <div className={styles.page_menu_container}>
-                        <div className={styles.page_menu_body}>
-                            <Menu />
-                        </div>
-                    </div>
                 </div>
             </div>
         </nav>
