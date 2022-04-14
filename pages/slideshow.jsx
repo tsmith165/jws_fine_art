a1b35d978dc070334af1c38ac7ad6ae2d6d083d2
Torrey Smith <tas.smity@yahoo.com>
2022-04-13 22:13:16 -0700

    added basic slideshow

---
+++ pages/slideshow.jsx
@@ -0,0 +1,43 @@
+import { prisma } from '../lib/prisma'
+
+import PageLayout from '../src/components/layout/PageLayout'
+import SlideshowComponent from '../src/components/Slideshow';
+
+import styles from '../styles/components/Slideshow.module.scss'
+
+export default function Slideshow({piece_list}) {
+  console.log("CURRENT PIECE LIST (NEXT LINE):")
+  console.log(piece_list);
+
+  return (
+    <PageLayout>
+      <div className={styles.slideshow_container}>
+        <SlideshowComponent piece_list={piece_list}/>
+      </div>
+    </PageLayout>
+  )
+}
+
+async function fetchPieces() {
+  console.log(`Fetching pieces with prisma`)
+  const piece_list = await prisma.piece.findMany({
+      orderBy: {
+          o_id: 'desc',
+      },
+  })
+
+  return piece_list
+}
+
+export const getStaticProps = async (context) => {
+  console.log("Getting Static Props")
+  const piece_list = await fetchPieces()
+
+  //console.log(context)
+  return { 
+      props: {
+          "piece_list": piece_list
+      },
+      revalidate: 60
+  }
+}
