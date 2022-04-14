0c0944d59256ab7b7dd5e91896b9b99033691b5a
Torrey Smith <tas.smity@yahoo.com>
2022-04-13 22:13:16 -0700

    added basic slideshow

---
+++ src/components/Slideshow.js
@@ -0,0 +1,101 @@
+import { useState, useEffect } from 'react';
+import Image from 'next/image'
+
+import styles from '../../styles/components/Slideshow.module.scss'
+
+import CloseIcon from '@material-ui/icons//Close';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import PlayArrow from '@material-ui/icons/PlayArrow';
+import Pause from '@material-ui/icons/Pause';
+import ArrowForwardIosRoundedIcon from '@material-ui/icons/ArrowForwardIosRounded';
+
+const baseURL = "https://jwsfineart.s3.us-west-1.amazonaws.com";
+
+const SlideshowComponent = ({piece_list}) => {
+    const debug = false;
+    if (debug) {
+        console.log(`WINDOW WIDTH: ${window_size.width} | HEIGHT: ${window_size.height}`);
+        console.log("Generating Gallery - Data Next Line")
+        console.log(piece_list)
+    }
+
+    console.log("Piece List (Next Line):")
+    console.log(piece_list)
+
+    const [cur_piece_id, set_cur_piece_id] = useState(0)
+    const [running, set_running] = useState(false)
+
+    console.log(`Running: ${running}`)
+    console.log(`cur_piece_id: ${cur_piece_id}`)
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            if (running) {
+                console.log(`Cur Piece Id: ${cur_piece_id} `)
+                if (cur_piece_id + 1 < piece_list.length - 1) {
+                    set_cur_piece_id(cur_piece_id + 1)
+                } else {
+                    set_cur_piece_id(0)
+                }
+            }
+        }, 2000)
+        return () => clearTimeout(timer)
+    })
+    
+    function handle_arrow_click(event, dir) {
+        if (running) set_running(false);
+        if (dir == true) {
+            if (cur_piece_id + 1 < piece_list.length - 1) {
+                set_cur_piece_id(cur_piece_id + 1)
+            } else {
+                set_cur_piece_id(0)
+            }
+        } else {
+            if (cur_piece_id - 1 > 0) {
+                set_cur_piece_id(cur_piece_id - 1)
+            } else {
+                set_cur_piece_id(piece_list.length - 1)
+            }
+        }
+    }
+
+    return (
+        <div className={styles.slideshow_body}>
+            <div className={styles.slideshow_image_container}>
+                <Image                                         
+                    className={styles.slideshow_image}
+                    src={`${baseURL}${piece_list[cur_piece_id]['image_path']}`}
+                    alt={piece_list[cur_piece_id]['title']}
+                    priority={true}
+                    layout='fill'
+                    objectFit='contain'
+                    quality={100} 
+                />
+            </div>
+            <div className={styles.slideshow_menu_container}>
+                <div className={styles.slideshow_menu_left_container}>
+                    <div className={styles.icon_container}>
+                        {running == true ? (
+                            <Pause className={`${styles.slideshow_icon}`} onClick={(e) => {e.preventDefault(); set_running(false)}}/>
+                        ) : (
+                            <PlayArrow className={`${styles.slideshow_icon}`}  onClick={(e) => {e.preventDefault(); set_running(true)}}/>
+                        )}
+                    </div>
+                    <div className={styles.icon_container}>
+                        <ArrowForwardIosRoundedIcon className={`${styles.slideshow_icon} ${styles.img_hor_vert}`} onClick={(e) => {handle_arrow_click(e, false)}}/>
+                    </div>
+                    <div className={styles.icon_container}>
+                        <ArrowForwardIosRoundedIcon className={`${styles.slideshow_icon}`} onClick={(e) => {e.preventDefault(); handle_arrow_click(e, true)}}/>
+                    </div>
+                </div>
+                <div className={styles.slideshow_menu_title_container}>
+                    <div className={styles.slideshow_menu_title}>
+                        {piece_list[cur_piece_id]['title']}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SlideshowComponent;
