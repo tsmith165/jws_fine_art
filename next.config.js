c097d1b01611b57b059b83afac34f14550f6d2f7
Torrey Smith <tas.smity@yahoo.com>
2022-04-15 20:26:04 -0700

    setting minimum cache ttl for images to 7 days

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["jwsfineart.s3.us-west-1.amazonaws.com"]
+    domains: ["jwsfineart.s3.us-west-1.amazonaws.com"],
+      minimumCacheTTL: 60 * 60 * 24 * 7, //In seconds
   },
 }
 
