c26a57c7d2610246db6c27e7a450eef3d9ff9427
Torrey Smith <tas.smity@yahoo.com>
2022-04-15 22:33:21 -0700

    setting description meta for SEO

--- src/components/layout/PageLayout.js
+++ src/components/layout/PageLayout.js
@@ -23,7 +23,7 @@ const PageLayout = ({page_title="JWS Fine Art", use_maps_api=false, children}) =
 
             <Head>
                 <title>{page_title}</title>
-                <meta name="description" content="JWS Fine Art" />
+                <meta name="description" content="JWS Fine Art Gallery - Browse and Purchase online!" />
                 <link rel="icon" href="/JWS_ICON.png" />
                 <link rel="preconnect" href="https://fonts.gstatic.com"/>
                 <link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:wght@300&family=Lato:wght@300&display=swap" rel="stylesheet"/>
