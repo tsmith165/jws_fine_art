b9a3a212786656540b5b4192f8d057048a3e6028
Torrey Smith <tas.smity@yahoo.com>
2023-02-09 16:10:01 -0800

    changed edit page from static paths to dynamic slug

--- pages/checkout/[id].jsx
+++ pages/checkout/[id].jsx
@@ -21,7 +21,8 @@ async function fetchPieces() {
     return pieces
 }
 
-async function get_piece_id_from_path_o_id(PathOID, pieces) {
+function get_piece_id_from_path_o_id(PathOID, pieces) {
+
     for (var i=0; i < pieces.length; i++) {
         if (pieces[i]['o_id'].toString() == PathOID.toString()) {
             return i
