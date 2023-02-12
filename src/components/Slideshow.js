24993410e1d323c759340c1ecdef047b6f457b9e
Torrey Smith <tas.smity@yahoo.com>
2023-02-12 12:09:38 -0800

    made small formatting change for declaring initial piece_list and piece_list_length constants and logging

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -20,12 +20,10 @@ class SlideshowComponent extends React.Component {
         super(props);
 
         const piece_list = this.props.piece_list;
-
-        console.log(`getServerSideProps Pieces (Next Line):`)
-        console.log(piece_list)
-
         const piece_list_length = piece_list.length
-        console.log(`Pieces Length: ${piece_list_length}`)
+
+        console.log(`getServerSideProps piece_list length: ${piece_list_length} | Data (Next Line):`)
+        console.log(piece_list)
 
         var image_array = [];
         
