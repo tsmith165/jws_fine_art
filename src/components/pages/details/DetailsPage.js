fed44517a4446385d334e52b18bfe82fe89d93cd
Torrey Smith <tas.smity@yahoo.com>
2023-02-11 16:56:15 -0800

    Testing loading all pieces on page load

--- src/components/pages/details/DetailsPage.js
+++ src/components/pages/details/DetailsPage.js
@@ -28,12 +28,40 @@ class DetailsPage extends React.Component {
         const pieces = this.props.pieces
         const pieces_length = this.props.pieces.length
         console.log(`Pieces Length: ${pieces_length}`)
+        
+        var image_array = [];
+        var current_piece = null;
+        var piece_position = 0;
+        var piece_db_id = null;
+
+        var title = '';
+        var type = '';
+        var description = '';
+        var sold = false;
+        var price = 9999;
+        var width = '';
+        var height = '';
+        var real_width = '';
+        var real_height = '';
+        var image_path = '';
+        var instagram = '';
 
         if (pieces_length > 0) {
             const piece_position = 0
             const current_piece = this.props.pieces[piece_position]
 
-            var image_array = [];
+            title =       (current_piece['title']       !== undefined) ? current_piece['title'] : ''
+            type =        (current_piece['type']        !== undefined) ? current_piece['type'] : ''
+            description = (current_piece['description'] !== undefined) ? current_piece['description'] : ''
+            sold =        (current_piece['sold']        !== undefined) ? current_piece['sold'] : ''
+            price =       (current_piece['price']       !== undefined) ? current_piece['price'] : ''
+            width =       (current_piece['width']       !== undefined) ? current_piece['width'] : ''
+            height =      (current_piece['height']      !== undefined) ? current_piece['height'] : ''
+            real_width =  (current_piece['real_width']  !== undefined) ? current_piece['real_width'] : ''
+            real_height = (current_piece['real_height'] !== undefined) ? current_piece['real_height'] : ''
+            image_path =  (current_piece['image_path']  !== undefined) ? `${baseURL}${current_piece['image_path']}` : ''
+            instagram =   (current_piece['instagram']   !== undefined) ? current_piece['instagram'] : ''
+
             for (var i=0; i < pieces.length; i++) {
                 let piece = pieces[i];
                 image_array.push((
@@ -51,87 +79,37 @@ class DetailsPage extends React.Component {
                     />
                 ))
             }
-    
-            const sold = current_piece["sold"]
-            const sold_html = (sold == true) ? 
-                ( 
-                    <b className={styles.piece_sold}>Sold</b> 
-                ) : ( 
-                    <Link href={`/checkout/${o_id}`}>
-                        <button className={styles.buy_now_button}>Buy</button>
-                    </Link> 
-                )
-        
-            const price = current_piece['price']
-            const price_html = (current_piece["sold"] == false) ? ( <b className={styles.price_text}>{`$${current_piece['price']}`}</b> ) : ( null )
+        }
 
-            console.log(`IMAGE PATH: ${current_piece['image_path']}`)
-    
-            // Don't call this.setState() here!
-            this.state = {
-                debug: false,
-                loading: true,
-                url_o_id: props.id,
-                pieces: pieces,
-                image_array: image_array,
-                current_piece: current_piece,
-                piece_position: piece_position,
-                piece_db_id: (current_piece['id'] !== undefined) ? current_piece['id'] : '',
-                piece_details: {
-                    title:       (current_piece['title']       !== undefined) ? current_piece['title'] : '',
-                    type:        (current_piece['type']        !== undefined) ? current_piece['type'] : '',
-                    description: (current_piece['description'] !== undefined) ? current_piece['description'] : '',
-                    sold:        (current_piece['sold']        !== undefined) ? current_piece['sold'] : '',
-                    price:       (current_piece['price']       !== undefined) ? current_piece['price'] : '',
-                    width:       (current_piece['width']       !== undefined) ? current_piece['width'] : '',
-                    height:      (current_piece['height']      !== undefined) ? current_piece['height'] : '',
-                    real_width:  (current_piece['real_width']  !== undefined) ? current_piece['real_width'] : '',
-                    real_height: (current_piece['real_height'] !== undefined) ? current_piece['real_height'] : '',
-                    image_path:  (current_piece['image_path']  !== undefined) ? `${baseURL}${current_piece['image_path']}` : '',
-                    instagram:   (current_piece['instagram']   !== undefined) ? current_piece['instagram'] : '',
-                },
-                image_url: '',
-                next_oid: (piece_position + 1 > pieces_length - 1) ? pieces[0]['o_id'] : pieces[piece_position + 1]['o_id'],
-                last_oid: (piece_position - 1 < 0) ? pieces[pieces_length - 1]['o_id'] : pieces[piece_position - 1]['o_id'],
-                description: (current_piece['description'] !== undefined) ? current_piece['description'] : '',
-                full_screen: false,
-                price: price,
-                price_html: price_html,
+        this.state = {
+            debug: false,
+            loading: true,
+            url_o_id: props.id,
+            pieces: pieces,
+            image_array: image_array,
+            current_piece: current_piece,
+            piece_position: piece_position,
+            piece_db_id: piece_db_id,
+            piece_details: {
+                title: title,
+                type: type,
+                description: description,
                 sold: sold,
-                sold_html: sold_html
-            };     
-        } else {
-            this.state = {
-                debug: false,
-                loading: true,
-                url_o_id: props.id,
-                pieces: null,
-                piece_position: null,
-                piece_db_id: null,
-                current_piece: null,
-                piece_details: {
-                    title:       '',
-                    type:        '',
-                    description: '',
-                    sold:        '',
-                    price:       '',
-                    width:       0,
-                    height:      0,
-                    real_width:  '',
-                    real_height: '',
-                    image_path:  '',
-                    instagram:   '',
-                },
-                image_url: '',
-                next_oid: null,
-                last_oid: null,
-                full_screen: false,
-                price: 0,
-                price_html: null,
-                sold: false,
-                sold_html: null,
-                description: ''
-            }; 
+                price: price,
+                width: width,
+                height: height,
+                real_width: real_width,
+                real_height: real_height,
+                image_path: image_path,
+                instagram: instagram,
+            },
+            image_url: '',
+            next_oid: (piece_position + 1 > pieces_length - 1) ? pieces[0]['o_id'] : pieces[piece_position + 1]['o_id'],
+            last_oid: (piece_position - 1 < 0) ? pieces[pieces_length - 1]['o_id'] : pieces[piece_position - 1]['o_id'],
+            description: description,
+            price: price,
+            sold: sold,
+            full_screen: false
         }
 
         this.fetch_pieces = this.fetch_pieces_from_api.bind(this);
@@ -284,8 +262,22 @@ class DetailsPage extends React.Component {
                             <div className={styles.details_form_container}>
                                 <div className={styles.details_navigation_container}>
                                     <div className={styles.details_navigation_inner_container}>
-                                        {this.state.price_html}
-                                        {this.state.sold_html}
+                                        {
+                                            (this.state.sold == false) ? ( 
+                                                <b className={styles.price_text}>{`$${this.state.price}`}</b> 
+                                            ) : ( 
+                                                null
+                                            )
+                                        }
+                                        {
+                                            (this.state.sold == true) ? ( 
+                                                <b className={styles.piece_sold}>Sold</b> 
+                                            ) : ( 
+                                                <Link href={`/checkout/${this.state.url_o_id}`}>
+                                                    <button className={styles.buy_now_button}>Buy</button>
+                                                </Link> 
+                                            )
+                                        }
                                         {
                                             (this.state.sold == true) ? 
                                                 (
