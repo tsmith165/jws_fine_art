4ddd732bd2ad73a740bd57ea31b56a3599f27a3e
Torrey Smith <tas.smity@yahoo.com>
2023-02-13 14:23:19 -0800

    Setting menu open to false when clicking menu link

--- src/components/layout/MenuOverlayButton.js
+++ src/components/layout/MenuOverlayButton.js
@@ -1,8 +1,12 @@
 import styles from '../../../styles/layout/MenuOverlay.module.scss'
 import Link from 'next/link'
 import { useClerk } from "@clerk/clerk-react";
+import {useRouter} from 'next/router'
 
 const MenuOverlayButton = ({id, menu_name, url_endpoint, set_menu_open}) => {
+    const router = useRouter();
+    router.prefetch(url_endpoint);
+
     if (menu_name == 'Sign Out') {
         const { signOut } = useClerk();
         return (
@@ -12,11 +16,9 @@ const MenuOverlayButton = ({id, menu_name, url_endpoint, set_menu_open}) => {
         )
     }
     return (
-        <Link href={url_endpoint} onClick={set_menu_open(false)}> 
-            <div className={styles.menu_overlay_item} id={id}>
-                <b className={styles.menu_overlay_item_title}>{menu_name}</b>
-            </div>
-        </Link>
+        <div className={styles.menu_overlay_item} id={id} onClick={ (e) => {e.preventDefault(); set_menu_open(false); router.push(url_endpoint)} }>
+            <b className={styles.menu_overlay_item_title}>{menu_name}</b>
+        </div>
     )
 }
 
