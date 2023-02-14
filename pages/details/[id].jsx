61f57788bcea5df1d9ff106eaa1dd6f77db76c35
Torrey Smith <tas.smity@yahoo.com>
2023-02-13 23:08:09 -0800

    switching from 'For Sale' -> 'Available'

--- pages/details/[id].jsx
+++ pages/details/[id].jsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import { useUser } from "@clerk/clerk-react";
 
 import { prisma } from '../../lib/prisma'
 
@@ -7,6 +8,8 @@ import DetailsPage from '../../src/components/pages/details/DetailsPage';
 const baseURL = "https://jwsfineartpieces.s3.us-west-1.amazonaws.com";
 
 const Details = ({piece_list}) => {
+    const { isLoaded, isSignedIn, user } = useUser();
+
     const router = useRouter();
     const id = router.query.id;
     console.log(`Page ID: ${id}`);
@@ -14,8 +17,13 @@ const Details = ({piece_list}) => {
     console.log(`Passing piece_list (Next Line): `)
     console.log(piece_list)
 
+    console.log(`Loaded: ${isLoaded} | Signed in: ${isSignedIn} ${(user != null) ?  `| User (next line):` : ``}`)
+    if (user != null) console.log(user)
+
     if (!router.isReady) return null
-    return ( <DetailsPage id={id} piece_list={piece_list} router={router}/> )
+    return ( 
+        <DetailsPage id={id} piece_list={piece_list} router={router} isLoaded={isLoaded} isSignedIn={isSignedIn} user={user}/> 
+    )
 }
 
 export default Details
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar'
 
 const Layout = (props) => {
+    console.log(`Loading layout with most recent page: ${props.most_recent_page_id}`)
     return (
         <>
             <Navbar most_recent_page_id={props.most_recent_page_id}/>
