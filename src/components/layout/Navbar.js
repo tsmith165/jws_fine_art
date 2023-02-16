88ff7adc3031687d3e5a7d15a93224055ef9fda4
Torrey Smith <tas.smity@yahoo.com>
2023-02-15 21:35:50 -0800

    Figured out passing state from child -> parent and moved filter menu to navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -12,11 +12,39 @@ import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 import { SignedIn, SignedOut, SignInButton, UserButton, ClerkLoading } from "@clerk/nextjs";
 
-const Navbar = ({most_recent_page_id}) => {
+import { Tooltip } from 'react-tooltip'
+
+import Tune from '@material-ui/icons/Tune';     // Filter Menu Toggle Button
+import AcUnit from '@material-ui/icons/AcUnit'; // Snow
+import Waves from '@material-ui/icons/Waves';   // Ocean
+import Landscape from '@material-ui/icons/Landscape'; // Mountains
+import LocationCity from '@material-ui/icons/LocationCity'; // City
+import LocalFlorist from '@material-ui/icons/LocalFlorist'; // Flowers
+import Portrait from '@material-ui/icons/Portrait'; // Portrait
+import Exposure from '@material-ui/icons/Exposure'; // Black And White
+import Block from '@material-ui/icons/Block'; // None
+import FilterBAndW from '@material-ui/icons/FilterBAndW'; // Abstract
+import ShoppingCart from '@material-ui/icons/ShoppingCart'; // Abstract
+
+
+const theme_filters = [
+    ['Water', <Waves className={styles.gallery_filter_icon} />], 
+    ['Snow', <AcUnit className={styles.gallery_filter_icon} />], 
+    ['Mountain', <Landscape className={styles.gallery_filter_icon} />], 
+    ['Landscape', <LocalFlorist className={styles.gallery_filter_icon} />], 
+    ['City', <LocationCity className={styles.gallery_filter_icon} />],
+    ['Portrait', <Portrait className={styles.gallery_filter_icon} />],
+    ['Black and White', <Exposure className={styles.gallery_filter_icon} />],
+    ['Abstract', <FilterBAndW className={styles.gallery_filter_icon} />],
+    ['Available', <ShoppingCart className={styles.gallery_filter_icon} />],
+    ['None', <Block className={styles.gallery_filter_icon} />]
+]
+
+const Navbar = ({most_recent_page_id, app_state, app_set_state}) => {
     const use_account_icon_as_link = true;
 
     const [menu_open, set_menu_open] = useState(false)
-
+    
     function menu_clicked(event) {
         event.preventDefault()
         console.log(`Setting Menu To: ${menu_open ? false : true}`)
@@ -29,6 +57,24 @@ const Navbar = ({most_recent_page_id}) => {
         set_menu_open(mouse_in)
     }
 
+    console.log(`Rendering Navbar with app_state URL Path: ${app_state.url_path} | Theme: ${app_state.theme} | Filter Menu Open: ${app_state.filter_menu_open}`)
+
+    var filter_menu_array = [];
+    for (var i = 0; i < theme_filters.length; i++) {
+        let filter = theme_filters[i][0];
+        let icon = theme_filters[i][1];
+        filter_menu_array.push((
+            <div className={(filter == app_state.theme) ? `${styles.gallery_filter_icon_container_selected} ${styles.gallery_filter_icon_container}` : styles.gallery_filter_icon_container} 
+                id={filter}
+                data-tooltip-content={`${filter}`}
+                onClick={(e) => { e.preventDefault(); app_set_state({theme: filter, url_path: app_state.url_path, filter_menu_open: true}) }}
+            >
+                {icon}
+                <Tooltip anchorId={filter} />
+            </div>
+        ))
+    }
+
     return (
         <nav className={styles.navbar}>
             <div className={styles.navbar_container}>
@@ -37,6 +83,23 @@ const Navbar = ({most_recent_page_id}) => {
                         <Image className={styles.navbar_logo_img} src='/jws_logo_small.png' alt='JWS Fine Art Logo' layout="fill" objectFit='contain' priority={true}/>
                     </div>
                 </Link>
+
+                { (app_state.url_path == '/') ? (
+                    <div className={(app_state.filter_menu_open == true) ? styles.gallery_filter_menu_toggle_open : styles.gallery_filter_menu_toggle}>
+                        {(app_state.filter_menu_open == true) ? (
+                            <div className={styles.gallery_filter_menu_container} >
+                                <div className={styles.gallery_filter_menu} >
+                                    { filter_menu_array }
+                                </div>
+                            </div>
+                        ) : (  
+                            <div className={styles.gallery_filter_menu_tooltip} onClick={(e) => { e.preventDefault(); app_set_state({theme: app_state.theme, url_path: app_state.url_path, filter_menu_open: !app_state.filter_menu_open}) }}>
+                                Filters
+                            </div>
+                        )}
+                        <Tune className={`${styles.gallery_filter_menu_toggle_icon}`} onClick={(e) => { e.preventDefault(); app_set_state({theme: app_state.theme, url_path: app_state.url_path, filter_menu_open: !app_state.filter_menu_open}) }}/>
+                    </div>
+                ) : ( null ) }
 
                 <Link href="https://www.instagram.com/jws_fineart/">
                     <div className={styles.instagram_link_container}>
