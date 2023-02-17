7c6a9f5a20f4a00c3939fc8a4f1e71dc7f611de5
Torrey Smith <tas.smity@yahoo.com>
2023-02-17 00:17:34 -0800

    fixed up mobile CSS for edit page a bit

--- pages/details/[id].jsx
+++ pages/details/[id].jsx
@@ -13,7 +13,7 @@ const Details = ({piece_list, most_recent_id, app_state, app_set_state}) => {
 
     if (!router.isReady) { return null }
     if (!isLoaded) { return ( <DetailsPage id={id} piece_list={piece_list} app_state={app_state} app_set_state={app_set_state} router={router} isSignedIn={false} user={null}/> ) }
-    
+
     // Not Signed In / User Does Not Exist
     if ((isSignedIn !== undefined && isSignedIn == true) && (user == undefined || user == null)) { 
         return ( <DetailsPage id={id} piece_list={piece_list} app_state={app_state} app_set_state={app_set_state} router={router} isSignedIn={false} user={null}/> )
@@ -27,8 +27,8 @@ export const getServerSideProps = async (context) => {
     var piece_list = await prisma.piece.findMany()
     piece_list.sort((a, b) => a['o_id'] - b['o_id']);
 
-    console.log(`Passing piece list (Next Line):`)
-    console.log(piece_list)
+    // console.log(`Passing piece list (Next Line):`)
+    // console.log(piece_list)
 
     return {
       props: {"piece_list": piece_list, "most_recent_id": piece_list[piece_list.length - 1]['id']}, // will be passed to the page component as props
