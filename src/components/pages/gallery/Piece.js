4cdc96917d06ed2d5d6e8e6da07502f566845553
Torrey Smith <tas.smity@yahoo.com>
2023-02-17 14:12:43 -0800

    refactoring switching off yellow dot in Piece.js

--- src/components/pages/gallery/Piece.js
+++ src/components/pages/gallery/Piece.js
@@ -2,7 +2,10 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../../../../styles/pages/Gallery.module.scss'
 
-const baseURL = "https://jwsfineartpieces.s3.us-west-1.amazonaws.com";
+const SITE_BASE_URL = "https://jwsfineartpieces.s3.us-west-1.amazonaws.com";
+
+// Setting to false as we are not using yello dot for unavailable currently
+const USING_YELLOW_DOT = false;
 
 const Piece = ({id, o_id, className, image_path, dimensions, title, description, sold, available}) => {
     var [x, y, img_width, img_height] = dimensions;
@@ -16,22 +19,22 @@ const Piece = ({id, o_id, className, image_path, dimensions, title, description,
             <Link href={`/details/${o_id}`}>
                 <div className={styles.piece_secondary_container} style={{width: img_width}}>
                     <div className={styles.piece_image_container}>
-                        <Image className={styles.piece_image} src={`${baseURL}${image_path}`} width={img_width} height={img_height} alt={title} style={{width: img_width, height: img_height}} sizes="(min-width: 799px) 30vw, (max-width: 800px) 45vw"/>
+                        <Image className={styles.piece_image} src={`${SITE_BASE_URL}${image_path}`} width={img_width} height={img_height} alt={title} style={{width: img_width, height: img_height}} sizes="(min-width: 799px) 30vw, (max-width: 800px) 45vw"/>
                     </div>
-                    { sold == false ? (
-                        ( available == true ? (
-                            null
-                        ) : (
-                            null
-                            /* <div className={styles.piece_sold_container}>
-                                <Image className={styles.piece_sold} src="/yellowDot.png" alt="Piece Sold" layout='fixed' width={30} height={30}/> 
-                            </div> */
-                        ))
-                    ) : (
+                    { 
+                    (sold == true) ? (
                         <div className={styles.piece_sold_container}>
                             <Image className={styles.piece_sold} src="/redDot.png" alt="Piece Sold" width={30} height={30} priority={true}/> 
                         </div>
-                    )}
+                    ) : ( 
+                        (USING_YELLOW_DOT && available == false) ? ( 
+                            <div className={styles.piece_sold_container}>
+                                <Image className={styles.piece_sold} src="/yellowDot.png" alt="Piece Sold" layout='fixed' width={30} height={30}/> 
+                            </div>
+                        ) : (
+                            null
+                        ))
+                    }
                 </div>
             </Link>
         </div>
