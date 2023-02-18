4cda482f181ff38aca819cbb25889c7b15e92c0d
Torrey Smith <tas.smity@yahoo.com>
2023-02-17 16:14:44 -0800

    moved to using pathname for checking if page is gallery to display filter menu in navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import React, {useEffect}  from 'react';
+import React from 'react';
 
 import { SignedIn, SignedOut, UserButton, ClerkLoading } from "@clerk/nextjs";
 import { Tooltip } from 'react-tooltip'
