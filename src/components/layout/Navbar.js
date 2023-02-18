ee16b66a3254ff3424e6ba1cd1752521715dbecc
Torrey Smith <tas.smity@yahoo.com>
2023-02-17 19:14:45 -0800

    Switched back to non-class Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import Image from 'next/image'
 import Link from 'next/link'
+import Image from 'next/image'
+import { React } from 'react';
 
-import { SignedIn, SignedOut, SignInButton, UserButton, ClerkLoading } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton, ClerkLoading } from "@clerk/nextjs";
 import { Tooltip } from 'react-tooltip'
 
 import MenuOverlay from './menu/MenuOverlay'
@@ -41,124 +41,116 @@ const THEME_FILTERS = [
     ['None', <Block className={styles.gallery_filter_icon} />]
 ]
 
-class Navbar extends React.Component {
-    constructor(props) {
-        super(props);
+const Navbar = ({most_recent_page_id, app_state, app_set_state, isLoaded, isSignedIn, user }) => {
 
+    console.log(`Rendering Navbar with app_state URL Path: ${app_state.pathname} | Theme: ${app_state.theme} | Filter Menu Open: ${app_state.filter_menu_open}`)
+    console.log(app_state)
+
+    var filter_menu_array = [];
+    for (var i = 0; i < THEME_FILTERS.length; i++) {
+        let filter = THEME_FILTERS[i][0];
+        let icon = THEME_FILTERS[i][1];
+        filter_menu_array.push((
+            <div className={(filter == app_state.theme) ? `${styles.gallery_filter_icon_container_selected} ${styles.gallery_filter_icon_container}` : styles.gallery_filter_icon_container} 
+                id={filter}
+                data-tooltip-content={`${filter}`}
+                onClick={(e) => { e.preventDefault(); app_set_state({theme: filter, url_path: app_state.url_path, filter_menu_open: true, pathname: app_state.pathname}) }}
+            >
+                {icon}
+                <Tooltip anchorId={filter} />
+            </div>
+        ))
     }
 
-    async componentDidMount() {
-
-    }
-
-    render() {
-        var filter_menu_array = [];
-        for (var i = 0; i < THEME_FILTERS.length; i++) {
-            let filter = THEME_FILTERS[i][0];
-            let icon = THEME_FILTERS[i][1];
-            filter_menu_array.push((
-                <div key={i} className={(filter == this.props.app_state.theme) ? `${styles.gallery_filter_icon_container_selected} ${styles.gallery_filter_icon_container}` : styles.gallery_filter_icon_container} 
-                    id={filter}
-                    data-tooltip-content={`${filter}`}
-                    onClick={(e) => { e.preventDefault(); console.log(`Upating theme to: ${filter}`); this.props.app_set_state({theme: filter, menu_open: false, filter_menu_open: true, pathname: this.props.app_state.pathname}) }}
-                >
-                    {icon}
-                    <Tooltip anchorId={filter} />
-                </div>
-            ))
-        }
-
-        console.log(`Rendering Navbar with app state (Next Line):`)
-        console.log(this.props.app_state)
-
-        return (
-            <nav className={styles.navbar}>
-                <div className={styles.navbar_container}>
-                    <div className={styles.navbar_logo}>
-                        <Link href="/">
-                            <Image className={styles.navbar_logo_img} src='/jws_logo_small.png' alt='JWS Fine Art Logo' priority={true} fill style={{objectFit:"contain"}} sizes="250px"/>
-                        </Link>
-                    </div>
-
-                    { ( this.props.app_state.pathname === '/') ? (
-                        <div className={(this.props.app_state.filter_menu_open == false) ? styles.gallery_filter_menu_toggle : `${styles.gallery_filter_menu_toggle} ${styles.gallery_filter_menu_toggle_open}` }>
-                            {(this.props.app_state.filter_menu_open == false) ? (
-                                <div className={styles.gallery_filter_menu_tooltip} onClick={(e) => { e.preventDefault(); this.props.app_set_state({...this.props.app_state, filter_menu_open: !this.props.app_state.filter_menu_open}) }}>
-                                    Filters
-                                </div>
-                            ) : ( null ) }
-                            <Tune className={styles.gallery_filter_menu_toggle_icon} onClick={(e) => { e.preventDefault(); this.props.app_set_state({...this.props.app_state, filter_menu_open: !this.props.app_state.filter_menu_open}) }}/>
-                        </div>
-                    ) : ( null ) }
-
-                    { ( this.props.app_state.pathname === '/' && this.props.app_state.filter_menu_open == true) ? (
-                        <div className={styles.gallery_filter_menu} >
-                            { filter_menu_array }
-                        </div>
-                    ) : ( null ) }
-
-                    <Link href="https://www.instagram.com/jws_fineart/">
-                        <div className={styles.instagram_link_container}>
-                            <div className={styles.instagram_logo_link_container}>
-                                <Image className={styles.instagram_logo_link} src='/instagram_icon_50.png' alt='Instagram Link' fill style={{objectFit:"contain"}} sizes='50px'/>
-                            </div>
-                        </div>
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.navbar_container}>
+                <div className={styles.navbar_logo}>
+                    <Link href="/" styles={{}}>
+                        <Image className={styles.navbar_logo_img} src='/jws_logo_small.png' alt='JWS Fine Art Logo' priority={true} fill style={{objectFit:"contain"}} sizes="250px"/>
                     </Link>
-
-                    
-                    <div className={styles.clerk_user_button_container}>
-                        <SignedIn>
-                            <UserButton
-                                appearance={{
-                                userProfile: { elements: { breadcrumbs: "bg-slate-500" } },
-                                }}
-                            />
-                        </SignedIn>
-                        <SignedOut>
-                            <Link href="/signin">
-                                <div className={styles.sign_in_container}>
-                                    <AccountCircleIcon className={styles.sign_in_button} />
-                                </div>
-                            </Link>
-                        </SignedOut>
-                        <ClerkLoading>
-                            <Link href="/signin">
-                                <div className={styles.sign_in_container}>
-                                    <AccountCircleIcon className={styles.sign_in_button} />
-                                </div>
-                            </Link>
-                        </ClerkLoading>
-                    </div>
-
-                    <div className={styles.page_menu_full_container} onMouseLeave={ (e) => { e.preventDefault(); this.props.app_set_state({...this.props.app_state, menu_open: false}) }}>
-                        <div className={styles.menu_button_container} onClick={ (e) => { e.preventDefault(); this.props.app_set_state({...this.props.app_state, menu_open: !this.props.app_state.menu_open}) }}>
-                            <MenuRoundedIcon className={(this.props.app_state.menu_open) ? styles.hamburger_button_open : styles.hamburger_button} />
-                        </div>
-                        
-                        {
-                            this.props.app_state.menu_open == true ? (
-                                <div className={styles.page_menu_container}>
-                                    <div className={styles.page_menu_body}>
-                                        <MenuOverlay 
-                                            most_recent_page_id={this.props.most_recent_page_id}
-                                            app_state={this.props.app_state} 
-                                            app_set_state={this.props.app_set_state} 
-                                            isLoaded={this.props.isLoaded} 
-                                            isSignedIn={this.props.isSignedIn} 
-                                            user={this.props.user}
-                                        />
-                                    </div>
-                                </div>
-                            ) : (
-                                null
-                            )
-                        }
-
-                    </div>
                 </div>
-            </nav>
-        )
-    }
+
+                { (app_state.pathname == '/') ? (
+                    <div className={(app_state.filter_menu_open == false) ? styles.gallery_filter_menu_toggle : `${styles.gallery_filter_menu_toggle} ${styles.gallery_filter_menu_toggle_open}` }>
+                        {(app_state.filter_menu_open == false) ? (
+                            <div className={styles.gallery_filter_menu_tooltip} onClick={(e) => { e.preventDefault(); app_set_state({filter_menu_open: !app_state.filter_menu_open, theme: app_state.theme, pathname: app_state.pathname}) }}>
+                                Filters
+                            </div>
+                        ) : ( null ) }
+                        <Tune className={styles.gallery_filter_menu_toggle_icon} onClick={(e) => { e.preventDefault(); app_set_state({filter_menu_open: !app_state.filter_menu_open, theme: app_state.theme, pathname: app_state.pathname}) }}/>
+                    </div>
+                ) : ( null ) }
+
+                {(app_state.pathname == '/' && app_state.filter_menu_open == true) ? (
+                    <div className={styles.gallery_filter_menu} >
+                        { filter_menu_array }
+                    </div>
+                ) : ( null ) }
+
+                <Link href="https://www.instagram.com/jws_fineart/">
+                    <div className={styles.instagram_link_container}>
+                        <div className={styles.instagram_logo_link_container}>
+                            <Image className={styles.instagram_logo_link} src='/instagram_icon_50.png' alt='Instagram Link' layout="fill" objectFit='contain'/>
+                        </div>
+                    </div>
+                </Link>
+
+                
+                <div className={styles.clerk_user_button_container}>
+                    <SignedIn>
+                        <UserButton
+                            appearance={{
+                            userProfile: { elements: { breadcrumbs: "bg-slate-500" } },
+                            }}
+                        />
+                    </SignedIn>
+                    <SignedOut>
+                        <Link href="/signin">
+                            <div className={styles.sign_in_container}>
+                                <AccountCircleIcon className={styles.sign_in_button} />
+                            </div>
+                        </Link>
+                    </SignedOut>
+                    <ClerkLoading>
+                        <Link href="/signin">
+                            <div className={styles.sign_in_container}>
+                                <AccountCircleIcon className={styles.sign_in_button} />
+                            </div>
+                        </Link>
+                    </ClerkLoading>
+                </div>
+                
+
+                                
+                <div className={styles.page_menu_full_container} onMouseLeave={ (e) => { e.preventDefault(); app_set_state({...app_state, menu_open: false}) }}>
+                    <div className={styles.menu_button_container} onClick={ (e) => { e.preventDefault(); app_set_state({...app_state, menu_open: !app_state.menu_open}) }}>
+                        <MenuRoundedIcon className={(app_state.menu_open) ? styles.hamburger_button_open : styles.hamburger_button} />
+                    </div>
+                    
+                    {
+                        app_state.menu_open == true ? (
+                            <div className={styles.page_menu_container}>
+                                <div className={styles.page_menu_body}>
+                                    <MenuOverlay 
+                                        most_recent_page_id={most_recent_page_id} 
+                                        app_state={app_state} 
+                                        app_set_state={app_set_state} 
+                                        isLoaded={isLoaded} 
+                                        isSignedIn={isSignedIn} 
+                                        user={user}
+                                    />
+                                </div>
+                            </div>
+                        ) : (
+                            null
+                        )
+                    }
+
+                </div>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar;
