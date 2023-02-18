5ff0f7ff31a74d0cebb1d33698ac0cba26cd2d4a
Torrey Smith <tas.smity@yahoo.com>
2023-02-17 19:42:46 -0800

    moved from using props in Layout.js to pulling each prop individually

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -4,23 +4,20 @@ import Navbar from './Navbar'
 import React from 'react';
 import { useUser } from "@clerk/clerk-react";
 
-const Layout = (props) => {
+const Layout = ( {children, most_recent_page_id, app_state, app_set_state} ) => {
     const { isLoaded, isSignedIn, user } = useUser();
-
-    console.log(`Layout App State (Next Line)`)
-    console.log(props)
 
     return (
         <>
             <Navbar 
-                most_recent_page_id={props.most_recent_page_id} 
-                app_state={props.app_state} 
-                app_set_state={props.app_set_state} 
+                most_recent_page_id={most_recent_page_id} 
+                app_state={app_state} 
+                app_set_state={app_set_state} 
                 isLoaded={isLoaded} 
                 isSignedIn={isSignedIn} 
                 user={user}
             />
-            {React.cloneElement(props.children, {isLoaded: isLoaded, isSignedIn: isSignedIn, user: user})}
+            {React.cloneElement(children, {isLoaded: isLoaded, isSignedIn: isSignedIn, user: user})}
         </>
     )
 }
