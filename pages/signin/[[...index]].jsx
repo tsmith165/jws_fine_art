cdf08b47d959eeb7567247f0ed6f94ee41c67314
Torrey Smith <tas.smity@yahoo.com>
2023-02-18 22:43:12 -0800

    fixing indentation on signin page

--- pages/signin/[[...index]].jsx
+++ pages/signin/[[...index]].jsx
@@ -9,25 +9,25 @@ import PageLayout from '../../src/components/layout/PageLayout'
 import { SignIn } from "@clerk/nextjs";
 
 class SignInPage extends React.Component {
-  constructor(props) {
-      super(props);
+    constructor(props) {
+        super(props);
 
-      this.page_title = "Sign In"
-  }
+        this.page_title = "Sign In"
+    }
 
-  async componentDidMount() { }
+    async componentDidMount() { }
 
-  render() {
-      return (
-        <PageLayout page_title={this.page_title}>
-            <div className={styles.sign_in_container}>
-                <div className={styles.sign_in_inner_container}>
-                    <SignIn path="/signin" routing="path" signUpUrl="/signup" />
+    render() {
+        return (
+            <PageLayout page_title={this.page_title}>
+                <div className={styles.sign_in_container}>
+                    <div className={styles.sign_in_inner_container}>
+                        <SignIn path="/signin" routing="path" signUpUrl="/signup" />
+                    </div>
                 </div>
-            </div>
-        </PageLayout>
-      )
-  }
+            </PageLayout>
+        )
+    }
 }
 
 export default SignInPage;
@@ -35,14 +35,14 @@ export default SignInPage;
 export const getServerSideProps = async (context) => {
     console.log("Getting Server Side Props")
     const piece = await prisma.piece.findFirst({
-      orderBy: {
-          o_id: 'desc',
-      },
+        orderBy: {
+            o_id: 'desc',
+        },
     })
   
     return { 
-      props: {
-        "most_recent_id": piece['id']
-      }
+        props: {
+            "most_recent_id": piece['id']
+        }
     }
 }
