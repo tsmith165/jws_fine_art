dd6afd70e49f7ec0b75f75177a268359188f7e77
Torrey Smith <tas.smity@yahoo.com>
2023-02-18 22:20:08 -0800

    Fixed Users page and updated indentation on manage / orders / slideshow

--- pages/users.jsx
+++ pages/users.jsx
@@ -47,71 +47,20 @@ class Users extends React.Component {
     }
 }
 
-// using client side session retrieval
-const Users = ({ user_tree_data, isLoaded, isSignedIn, user }) => {
-  const router = useRouter()
-  const refresh_data = () => {
-    console.log("Refreshing Data...")
-    router.replace(router.asPath)
-  }
-
-  if (!isLoaded) {
-    return(<></>)
-  }
-  if (!isSignedIn) {
-    router.push('/')
-  }
-  if (user == null) {
-    router.push('/')
-  }
-  const role = user.publicMetadata.role;
-  console.log(`USER ROLE: ${role}`)
-  if (user.publicMetadata.role !== "ADMIN") {
-    router.push('/')
-  }
-
-  const page_jsx =  (
-    <div className={styles.admin_main_container}>
-      <div className={styles.user_tree_container}>
-        <UserTree user_tree_data={user_tree_data}
-                  refresh_data={refresh_data}
-        />
-      </div>
-    </div>
-  );
-
-  return (
-    <PageLayout page_title={"Admin"}>
-      <div className={styles.main_container}>
-        <div className={styles.main_body}>
-          <h2 className={styles.module_title}>User Management:</h2>
-          <div className={styles.admin_main_container}>
-            <div className={styles.user_tree_container}>
-              <UserTree user_tree_data={user_tree_data}
-                        refresh_data={refresh_data}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </PageLayout>
-  )
-};
-
 export async function getServerSideProps() {
-  const users = await prisma.user.findMany();
-  console.log(`Retrieved users (Next Line):`)
-  console.log(users);
+    const users = await prisma.user.findMany();
+    console.log(`Retrieved users (Next Line):`)
+    console.log(users);
 
-  const piece = await prisma.piece.findFirst({
-    orderBy: {
-        o_id: 'desc',
-    },
-  })
+    const piece = await prisma.piece.findFirst({
+        orderBy: {
+            o_id: 'desc',
+        },
+    })
 
-  const user_tree_data = JSON.parse(JSON.stringify(users));
+    const user_tree_data = JSON.parse(JSON.stringify(users));
 
-  return { props: { "user_tree_data": user_tree_data, "most_recent_id": piece['id'] } }
+    return { props: { "user_tree_data": user_tree_data, "most_recent_id": piece['id'] } }
 
 }
 
