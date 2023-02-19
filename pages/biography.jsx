7a2b3a6a05ab31f2d4c65fbc170a1ef23634d9f0
Torrey Smith <tas.smity@yahoo.com>
2023-02-18 21:53:32 -0800

    refactored index / bio pages to class components

--- pages/biography.jsx
+++ pages/biography.jsx
@@ -1,27 +1,41 @@
+import React from 'react';
+
 import PageLayout from '../src/components/layout/PageLayout'
 import BiographyPage from '../src/components/pages/biography/BiographyPage'
 
 import { prisma } from '../lib/prisma'
 
-export default function Biography({}) {
-  return (
-    <PageLayout page_title={"Biography"}>
-      <BiographyPage/>
-    </PageLayout>
-  )
+class Biography extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.page_title = "Gallery JWS Fine Art"
+    }
+
+    async componentDidMount() { }
+
+    render() {
+        return (
+            <PageLayout page_title={"Biography"}>
+                <BiographyPage/>
+            </PageLayout>
+        )
+    }
 }
 
-export const getServerSideProps = async (context) => {
-  console.log("Getting Server Side Props")
-  const piece = await prisma.piece.findFirst({
-    orderBy: {
-        o_id: 'desc',
-    },
-  })
+export default Biography
 
-  return { 
-    props: {
-      "most_recent_id": piece['id']
+export const getServerSideProps = async (context) => {
+    console.log("Getting Server Side Props")
+    const piece = await prisma.piece.findFirst({
+        orderBy: {
+            o_id: 'desc',
+        },
+    })
+
+    return { 
+        props: {
+            "most_recent_id": piece['id']
+        }
     }
-  }
 }
