5253b3ea83d0d37cbafb713e1d54e7879ac88f9f
Torrey Smith <tas.smity@yahoo.com>
2023-02-19 14:32:31 -0800

    commiting changes made while cleaning up for adding to template-cli

--- src/components/pages/contact/Contact.js
+++ src/components/pages/contact/Contact.js
@@ -19,26 +19,26 @@ class Contact extends React.Component {
 
         return (
             <div className={styles.contact_container}>
-            <div className={styles.contact_image_container}>
-                <div className={styles.contact_image_border}>
-                    <Image
-                        className={styles.contact_image}
-                        src={"https://jwsfineartpieces.s3.us-west-1.amazonaws.com/site/bio_pic_small.jpg"}
-                        alt={"Bio Pic"}
-                        priority={true}
-                        width={200}
-                        height={267}
-                        quality={100}
-                    />
+                <div className={styles.contact_image_container}>
+                    <div className={styles.contact_image_border}>
+                        <Image
+                            className={styles.contact_image}
+                            src={"https://jwsfineartpieces.s3.us-west-1.amazonaws.com/site/bio_pic_small.jpg"}
+                            alt={"Bio Pic"}
+                            priority={true}
+                            width={200}
+                            height={267}
+                            quality={100}
+                        />
+                    </div>
+                </div>
+                <div className={styles.contact_title_container}>
+                    <b className={styles.contact_title}>Jill Weeks Smith</b>
+                </div>
+                <div className={styles.contact_text_container}>
+                    <a className={`${styles.contact_link} ${styles.link}`} href="mailto:jwsfineart@gmail.com">jwsfineart@gmail.com</a>
                 </div>
             </div>
-            <div className={styles.contact_title_container}>
-                <b className={styles.contact_title}>Jill Weeks Smith</b>
-            </div>
-            <div className={styles.contact_text_container}>
-                <a className={`${styles.contact_link} ${styles.link}`} href="mailto:jwsfineart@gmail.com">jwsfineart@gmail.com</a>
-            </div>
-        </div>
         )
     }
 }
