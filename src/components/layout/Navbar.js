5ab8812a6ef6ffb884a3d7743e14f5a15e78467e
Torrey Smith <tas.smity@yahoo.com>
2023-02-19 14:32:31 -0800

    commiting changes made while cleaning up for adding to template-cli

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import React, {useEffect}  from 'react';
+import React  from 'react';
 
 import { SignedIn, SignedOut, UserButton, ClerkLoading } from "@clerk/nextjs";
 import { Tooltip } from 'react-tooltip'
@@ -72,90 +72,90 @@ class Navbar extends React.Component {
 
         return (
             <nav className={styles.navbar}>
-            <div className={styles.navbar_container}>
-                <div className={styles.navbar_logo}>
-                    <Link href="/">
-                        <Image className={styles.navbar_logo_img} src='/jws_logo_small.png' alt='JWS Fine Art Logo' layout="fill" objectFit='contain' sizes="250px"/>
-                    </Link>
-                </div>
-
-                { ( this.props.app_state.pathname == '/') ? (
-                    <div className={(this.props.app_state.filter_menu_open == false) ? styles.gallery_filter_menu_toggle : `${styles.gallery_filter_menu_toggle} ${styles.gallery_filter_menu_toggle_open}` }>
-                        {(this.props.app_state.filter_menu_open == false) ? (
-                            <div className={styles.gallery_filter_menu_tooltip} onClick={(e) => { e.preventDefault(); this.props.app_set_state({...this.props.app_state, filter_menu_open: !this.props.app_state.filter_menu_open}) }}>
-                                Filters
-                            </div>
-                        ) : ( null ) }
-                        <Tune className={styles.gallery_filter_menu_toggle_icon} onClick={(e) => { e.preventDefault(); this.props.app_set_state({...this.props.app_state, filter_menu_open: !this.props.app_state.filter_menu_open}) }}/>
-                    </div>
-                ) : ( null ) }
-
-                { ( this.props.app_state.pathname == '/' && this.props.app_state.filter_menu_open == true) ? (
-                    <div className={styles.gallery_filter_menu} >
-                        { filter_menu_array }
-                    </div>
-                ) : ( null ) }
-
-                <Link href="https://www.instagram.com/jws_fineart/">
-                    <div className={styles.instagram_link_container}>
-                        <div className={styles.instagram_logo_link_container}>
-                            <Image className={styles.instagram_logo_link} src='/instagram_icon_50.png' alt='Instagram Link' layout="fill" objectFit='contain'/>
-                        </div>
-                    </div>
-                </Link>
-
-                
-                <div className={styles.clerk_user_button_container}>
-                    <SignedIn>
-                        <UserButton
-                            appearance={{
-                            userProfile: { elements: { breadcrumbs: "bg-slate-500" } },
-                            }}
-                        />
-                    </SignedIn>
-                    <SignedOut>
-                        <Link href="/signin">
-                            <div className={styles.sign_in_container}>
-                                <AccountCircleIcon className={styles.sign_in_button} />
-                            </div>
+                <div className={styles.navbar_container}>
+                    <div className={styles.navbar_logo}>
+                        <Link href="/">
+                            <Image className={styles.navbar_logo_img} src='/jws_logo_small.png' alt='JWS Fine Art Logo' layout="fill" objectFit='contain' sizes="250px"/>
                         </Link>
-                    </SignedOut>
-                    <ClerkLoading>
-                        <Link href="/signin">
-                            <div className={styles.sign_in_container}>
-                                <AccountCircleIcon className={styles.sign_in_button} />
-                            </div>
-                        </Link>
-                    </ClerkLoading>
-                </div>
-
-                <div className={styles.page_menu_full_container} onMouseLeave={ (e) => { e.preventDefault(); this.props.app_set_state({...this.props.app_state, menu_open: false}) }}>
-                    <div className={styles.menu_button_container} onClick={ (e) => { e.preventDefault(); this.props.app_set_state({...this.props.app_state, menu_open: !this.props.app_state.menu_open}) }}>
-                        <MenuIcon className={(this.props.app_state.menu_open) ? styles.hamburger_button_open : styles.hamburger_button} />
                     </div>
-                    
-                    {
-                        this.props.app_state.menu_open == true ? (
-                            <div className={styles.page_menu_container}>
-                                <div className={styles.page_menu_body}>
-                                    <MenuOverlay 
-                                        most_recent_page_id={this.props.most_recent_page_id}
-                                        app_state={this.props.app_state} 
-                                        app_set_state={this.props.app_set_state} 
-                                        isLoaded={this.props.isLoaded} 
-                                        isSignedIn={this.props.isSignedIn} 
-                                        user={this.props.user}
-                                    />
+
+                    { ( this.props.app_state.pathname == '/') ? (
+                        <div className={(this.props.app_state.filter_menu_open == false) ? styles.gallery_filter_menu_toggle : `${styles.gallery_filter_menu_toggle} ${styles.gallery_filter_menu_toggle_open}` }>
+                            {(this.props.app_state.filter_menu_open == false) ? (
+                                <div className={styles.gallery_filter_menu_tooltip} onClick={(e) => { e.preventDefault(); this.props.app_set_state({...this.props.app_state, filter_menu_open: !this.props.app_state.filter_menu_open}) }}>
+                                    Filters
                                 </div>
-                            </div>
-                        ) : (
-                            null
-                        )
-                    }
+                            ) : ( null ) }
+                            <Tune className={styles.gallery_filter_menu_toggle_icon} onClick={(e) => { e.preventDefault(); this.props.app_set_state({...this.props.app_state, filter_menu_open: !this.props.app_state.filter_menu_open}) }}/>
+                        </div>
+                    ) : ( null ) }
 
+                    { ( this.props.app_state.pathname == '/' && this.props.app_state.filter_menu_open == true) ? (
+                        <div className={styles.gallery_filter_menu} >
+                            { filter_menu_array }
+                        </div>
+                    ) : ( null ) }
+
+                    <Link href="https://www.instagram.com/jws_fineart/">
+                        <div className={styles.instagram_link_container}>
+                            <div className={styles.instagram_logo_link_container}>
+                                <Image className={styles.instagram_logo_link} src='/instagram_icon_50.png' alt='Instagram Link' layout="fill" objectFit='contain'/>
+                            </div>
+                        </div>
+                    </Link>
+
+                    
+                    <div className={styles.clerk_user_button_container}>
+                        <SignedIn>
+                            <UserButton
+                                appearance={{
+                                userProfile: { elements: { breadcrumbs: "bg-slate-500" } },
+                                }}
+                            />
+                        </SignedIn>
+                        <SignedOut>
+                            <Link href="/signin">
+                                <div className={styles.sign_in_container}>
+                                    <AccountCircleIcon className={styles.sign_in_button} />
+                                </div>
+                            </Link>
+                        </SignedOut>
+                        <ClerkLoading>
+                            <Link href="/signin">
+                                <div className={styles.sign_in_container}>
+                                    <AccountCircleIcon className={styles.sign_in_button} />
+                                </div>
+                            </Link>
+                        </ClerkLoading>
+                    </div>
+
+                    <div className={styles.page_menu_full_container} onMouseLeave={ (e) => { e.preventDefault(); this.props.app_set_state({...this.props.app_state, menu_open: false}) }}>
+                        <div className={styles.menu_button_container} onClick={ (e) => { e.preventDefault(); this.props.app_set_state({...this.props.app_state, menu_open: !this.props.app_state.menu_open}) }}>
+                            <MenuIcon className={(this.props.app_state.menu_open) ? styles.hamburger_button_open : styles.hamburger_button} />
+                        </div>
+                        
+                        {
+                            this.props.app_state.menu_open == true ? (
+                                <div className={styles.page_menu_container}>
+                                    <div className={styles.page_menu_body}>
+                                        <MenuOverlay 
+                                            most_recent_page_id={this.props.most_recent_page_id}
+                                            app_state={this.props.app_state} 
+                                            app_set_state={this.props.app_set_state} 
+                                            isLoaded={this.props.isLoaded} 
+                                            isSignedIn={this.props.isSignedIn} 
+                                            user={this.props.user}
+                                        />
+                                    </div>
+                                </div>
+                            ) : (
+                                null
+                            )
+                        }
+
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
         )
     }
 }
