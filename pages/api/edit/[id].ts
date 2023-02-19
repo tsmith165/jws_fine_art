3202469cdfb6e4e11098d0b7191879f0efa3cef6
Torrey Smith <tas.smity@yahoo.com>
2023-02-19 07:59:34 -0800

    Removed unused 'SECRET' env var and fixed issue with deconstructing get_piece_from_path_o_id() response as it was attempting to deconstruct the initial promise into [piece_postiion, current_piece]

--- pages/api/edit/[id].ts
+++ pages/api/edit/[id].ts
@@ -2,8 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../lib/prisma";
 import { clerkClient, getAuth } from "@clerk/nextjs/server";
 
-const secret = process.env.SECRET;
-
 export default async function handler(req:NextApiRequest, res: NextApiResponse) {
   if(req.method !== 'POST') {
     console.log("Request.method != POST.  Status: 402")
