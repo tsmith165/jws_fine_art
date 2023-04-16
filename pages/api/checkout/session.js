5bd3c9874d217f008c5177408e5c8401be98e5d6
Torrey Smith <tas.smity@yahoo.com>
2023-04-16 06:12:31 -0700

    changed stripe success / cancel urls to add 'https://'

--- pages/api/checkout/session.js
+++ pages/api/checkout/session.js
@@ -1,72 +1,84 @@
-import PROJECT_CONSTANTS from '@/lib/constants'
+import PROJECT_CONSTANTS from '@/lib/constants';
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
-    if(req.method !== 'POST') {
+    if (req.method !== 'POST') {
         // REQUEST METHOD NOT POST
-        console.log("Request.method != POST.  Status: 402")
-        res.status(402)
-        res.end()
-        return
-    } 
-    
-    console.log(`Checkout Session REQ.BODY (Next Line):`)
+        console.log('Request.method != POST.  Status: 402');
+        res.status(402);
+        res.end();
+        return;
+    }
+
+    console.log(`Checkout Session REQ.BODY (Next Line):`);
     console.log(req.body);
 
     if (!req.body) {
         // NO REQ.BODY JSON PASSED IN
-        console.log("Request.body not passed in.  Status: 405")
-        res.status(405)
-        res.end()
-        return
-    } 
+        console.log('Request.body not passed in.  Status: 405');
+        res.status(405);
+        res.end();
+        return;
+    }
 
-    const passed_json = req.body
-    console.log(`Passed JSON (Next Line):`)
+    const passed_json = req.body;
+    console.log(`Passed JSON (Next Line):`);
     console.log(passed_json);
 
-    const attrs_to_check = ['piece_db_id', 'piece_title', 'image_path', 'width', 'height', 'price', 'full_name', 'phone', 'email', 'address', 'international' ]
-    var attr_errors_found = false
+    const attrs_to_check = [
+        'piece_db_id',
+        'piece_title',
+        'image_path',
+        'width',
+        'height',
+        'price',
+        'full_name',
+        'phone',
+        'email',
+        'address',
+        'international',
+    ];
+    var attr_errors_found = false;
     for (var i = 0; i < attrs_to_check.length; i++) {
-        let attr = attrs_to_check[i]
+        let attr = attrs_to_check[i];
         if (passed_json[attr] == undefined) {
-            console.error(`Failed to pass in attribute: ${attr}`)
-            attr_errors_found = true
+            console.error(`Failed to pass in attribute: ${attr}`);
+            attr_errors_found = true;
         }
     }
 
     if (attr_errors_found) {
-        console.error("Request.body not fully passed in.  Exit status: 406")
-        res.status(406)
-        res.end()
-        return
+        console.error('Request.body not fully passed in.  Exit status: 406');
+        res.status(406);
+        res.end();
+        return;
     }
 
     // Create Stripe Session
-    const piece_db_id   = passed_json.piece_db_id;
-    const piece_title   = passed_json.piece_title;
-    const image_path    = passed_json.image_path;
-    const width         = passed_json.width;
-    const height        = passed_json.height;
-    const price         = passed_json.price;
-    const full_name     = passed_json.full_name;
-    const phone         = passed_json.phone;
-    const email         = passed_json.email;
-    const address       = passed_json.address;
+    const piece_db_id = passed_json.piece_db_id;
+    const piece_title = passed_json.piece_title;
+    const image_path = passed_json.image_path;
+    const width = passed_json.width;
+    const height = passed_json.height;
+    const price = passed_json.price;
+    const full_name = passed_json.full_name;
+    const phone = passed_json.phone;
+    const email = passed_json.email;
+    const address = passed_json.address;
     const international = passed_json.international;
 
-    var price_int = parseInt(price.toString().replace("$",""))
+    var price_int = parseInt(price.toString().replace('$', ''));
     const converted_price = `${price_int}00`;
 
     if (international == true || international == 'true') {
-        console.log(`Adding $30 to current price of ${price_int}`)
+        console.log(`Adding $30 to current price of ${price_int}`);
         price_int = price_int + 30;
-        console.log(`Price after adding international shipping: ${price_int}`)
+        console.log(`Price after adding international shipping: ${price_int}`);
     }
 
-    var full_image_url = `${image_path}`
-    console.log(`Piece Title: ${piece_title} | Price: ${converted_price} | Image Path: ${full_image_url}`)
+    var full_image_url = `${image_path}`;
+    console.log(`Piece Title: ${piece_title} | Price: ${converted_price} | Image Path: ${full_image_url}`);
 
     const params = {
         payment_method_types: ['card'],
@@ -81,7 +93,7 @@ export default async function handler(req, res) {
                     },
                     unit_amount: converted_price,
                 },
-            }
+            },
         ],
         payment_intent_data: {
             metadata: {
@@ -90,21 +102,20 @@ export default async function handler(req, res) {
                 price_id: price,
                 image_path: full_image_url,
                 image_width: width,
-                image_height: height
-
-            }
+                image_height: height,
+            },
         },
         mode: 'payment',
-        success_url: `${PROJECT_CONSTANTS.SITE_URL}/success/${piece_db_id}`,
-        cancel_url: `${PROJECT_CONSTANTS.SITE_URL}/cancel/${piece_db_id}`,
-    }
+        success_url: `https://${PROJECT_CONSTANTS.SITE_URL}/success/${piece_db_id}`,
+        cancel_url: `https://${PROJECT_CONSTANTS.SITE_URL}/cancel/${piece_db_id}`,
+    };
 
-    console.log("Attempting to create checkout session with following params (Next Line):")
-    console.log(params)
+    console.log('Attempting to create checkout session with following params (Next Line):');
+    console.log(params);
     const checkout_session = await stripe.checkout.sessions.create(params);
 
-    console.log(`Session ID: ${checkout_session.id}`)
+    console.log(`Session ID: ${checkout_session.id}`);
 
     res.status(200).json({ id: checkout_session.id });
-    res.end()
+    res.end();
 }
