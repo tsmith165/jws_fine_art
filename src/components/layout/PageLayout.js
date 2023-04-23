2392fc8e16327dcef62c728ba34ed72fabe195a9
Torrey Smith <tas.smity@yahoo.com>
2023-04-22 21:50:50 -0700

    Attempting to fix double google maps api import callback issue

--- src/components/layout/PageLayout.js
+++ src/components/layout/PageLayout.js
@@ -1,38 +1,32 @@
-import PROJECT_CONSTANTS from '@/lib/constants'
+import PROJECT_CONSTANTS from '@/lib/constants';
 
-import Head from 'next/head'
-import Script from 'next/script'
-import styles from "@/styles/layout/PageLayout.module.scss"
+import Head from 'next/head';
+import Script from 'next/script';
+import styles from '@/styles/layout/PageLayout.module.scss';
 
-const PageLayout = ({page_title=PROJECT_CONSTANTS.SITE_FULL_NAME, use_maps_api=false, children}) => {
-    console.log(`Loading Page Layout With Title: ${page_title}`)
+const PageLayout = ({ page_title = PROJECT_CONSTANTS.SITE_FULL_NAME, use_maps_api = false, children }) => {
+    console.log(`Loading Page Layout With Title: ${page_title}`);
 
     return (
         <div className={styles.container}>
-            {
-                (use_maps_api == true) ? (
-                    <Script 
-                        strategy="beforeInteractive" 
-                        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCvrLDFUzjxCnKIDSuPwBYEbfnWrrIUnu4&libraries=places"
-                    />
-                ) : (
-                    null
-                )
-            }
+            {use_maps_api == true ? (
+                <Script
+                    strategy="beforeInteractive"
+                    src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places"
+                    onLoad={() => {}}
+                />
+            ) : null}
 
             <Head>
-                <meta charSet="UTF-8"/>
+                <meta charSet="UTF-8" />
                 <meta name="description" content={PROJECT_CONSTANTS.SITE_DESCRIPTION} />
                 <title>{page_title}</title>
                 <link rel="icon" href="/JWS_ICON.png" />
             </Head>
-            
-            <main className={styles.main}>
-                {children}
-            </main>
+
+            <main className={styles.main}>{children}</main>
         </div>
-    )
-}
+    );
+};
 
 export default PageLayout;
-
