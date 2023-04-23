f1f5ecb2f690ec4a29860a763567139b6adcf383
Torrey Smith <tas.smity@yahoo.com>
2023-04-22 22:18:34 -0700

    Setting google maps callback to default initMap

--- src/components/layout/PageLayout.js
+++ src/components/layout/PageLayout.js
@@ -7,17 +7,12 @@ import styles from '@/styles/layout/PageLayout.module.scss';
 const PageLayout = ({ page_title = PROJECT_CONSTANTS.SITE_FULL_NAME, use_maps_api = false, children }) => {
     console.log(`Loading Page Layout With Title: ${page_title}`);
 
-    function initMap() {
-        // Your code to initialize the map goes here
-        // This code will only be executed after the Google Maps API is fully loaded
-    }
-
     return (
         <div className={styles.container}>
             {use_maps_api == true ? (
                 <Script
                     strategy="beforeInteractive"
-                    src={`https://maps.googleapis.com/maps/api/js?key=AIzaSyCvrLDFUzjxCnKIDSuPwBYEbfnWrrIUnu4&libraries=places&callback=initMap`}
+                    src={`https://maps.googleapis.com/maps/api/js?key=AIzaSyCvrLDFUzjxCnKIDSuPwBYEbfnWrrIUnu4&libraries=places`}
                 />
             ) : null}
 
