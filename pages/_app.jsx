c8934de19b42b09f73a27ce5a1abeea37587989c
Torrey Smith <tas.smity@yahoo.com>
2023-04-22 21:47:16 -0700

    Attempting to fix double google maps api import

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -51,10 +51,6 @@ const App = ({ Component, pageProps }) => {
         <ClerkProvider appearance={{ baseTheme: dark }}>
             <Script
                 strategy="afterInteractive"
-                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCvrLDFUzjxCnKIDSuPwBYEbfnWrrIUnu4&libraries=places&callback=Function.prototype"
-            />
-            <Script
-                strategy="afterInteractive"
                 src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
             />
             <Script
