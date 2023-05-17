4d9284162c06f7a9f8bf2b8760238ae5dd7ee5a2
Torrey Smith <tas.smity@yahoo.com>
2023-05-17 11:45:42 -0700

    Added TawkTo and linked with dashboard

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -69,6 +69,23 @@ const App = ({ Component, pageProps }) => {
           `,
                 }}
             />
+            <Script
+                id="tawkto-init"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/6465199bad80445890ed8909/1h0leo2ac';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+          `,
+                }}
+            />
             {!isPrivatePath && (
                 <Layout
                     most_recent_page_id={pageProps.most_recent_id}
