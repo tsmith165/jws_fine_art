5e809ec340bfe6234304ca01ea13777c5295aa05
tsmith165 <torreysmith165@gmail.com>
2023-11-19 07:24:02 -0800

    started converting admin to tailwind

--- app/admin/admin.jsx
+++ app/admin/admin.jsx
@@ -1,6 +1,5 @@
 'use client'
 
-import logger from '@/lib/logger';
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useUser } from '@clerk/nextjs';
@@ -11,7 +10,6 @@ function Admin(props) {
     const { isLoaded, isSignedIn, user } = useUser();
 
     const router = useRouter();
-    const page_title = 'Admin Dashboard';
     const [analyticsData, setAnalyticsData] = useState(null);
 
     useEffect(() => {
@@ -110,26 +108,28 @@ function Admin(props) {
     if (!isSignedIn) {
         console.log('User is not signed in.  Redirecting to signin page...')
         return router.push('/signin')
-    } 
+    }
     if (!user || user.publicMetadata?.role?.toLowerCase() != 'admin') {
         console.log(`Current user is not admin - Role: ${user.publicMetadata?.role?.toLowerCase() || 'none'}`)
         return router.push('/signin')
     }
 
     return (
-        <div className={styles.main_container}>
-            <div className={styles.main_body}>
-                <div className={styles.backup_panel}>
-                    <div className={styles.panel_header}>Data Backup</div>
-                    <div className={styles.panel_body}>
-                        <button className={styles.export_button} onClick={exportPiecesAsXLSX}>
+        <div className={'flex justify-center'}>
+            <div className={'flex flex-col items-center w-full h-full p-5 overflow-x-hidden'}>
+                <div className={'w-full pb-5 border-2 border-secondary rounded-md'}>
+                    <div className={'w-full h-[40px] px-1 py-2 bg-secondary text-light font-lato text-lg rounded-tl-sm rounded-tr-sm'}>
+                        Data Backup
+                    </div>
+                    <div className={'w-full bg-primary rounded-bl-sm rounded-br-sm p-5'}>
+                        <button className={'px-2 py-5 bg-light text-dark font-lato border-none rounded-md cursor-pointer uppercase'} onClick={exportPiecesAsXLSX}>
                             Export Pieces as XLSX
                         </button>
                     </div>
                 </div>
-                <div className={styles.analytics_panel}>
-                    <div className={styles.panel_header}>Analytics Data</div>
-                    <div className={styles.panel_body}>
+                <div className={'flex flex-col items-center w-full h-full p-5 overflow-x-hidden'}>
+                    <div className={'w-full h-[40px] px-1 py-2 bg-secondary text-light font-lato text-lg rounded-tl-sm rounded-tr-sm'}>Analytics Data</div>
+                    <div className={'w-full bg-primary rounded-bl-sm rounded-br-sm p-5'}>
                         {renderAnalyticsData()}
                     </div>
                 </div>
