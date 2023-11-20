eb9722b08df23badb15a8061a148f7d012e41482
Torrey Smith <tas.smity@yahoo.com>
2023-11-20 12:58:46 -0800

    small round of cleanup for tailwind transition

--- app/gallery/Piece.jsx
+++ app/gallery/Piece.jsx
@@ -1,51 +1,62 @@
-'use client'
+'use client';
 
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from 'next/image';
+import Link from 'next/link';
 
-import PROJECT_CONSTANTS from '@/lib/constants'
-
-import styles from '@/styles/pages/Gallery.module.scss'
+import PROJECT_CONSTANTS from '@/lib/constants';
 
 const USING_YELLOW_DOT = false;
 
 const Piece = ({ dimensions, id, className, o_id, image_path, title, sold, available }) => {
     var [x, y, img_width, img_height] = dimensions;
-    x = (x == null || x < 0) ? 0 : x;
+    x = x == null || x < 0 ? 0 : x;
 
     return (
-        <div id={id} className={`absolute p-1 rounded-md bg-tertiary ${className}`} style={{width: img_width + 8, height: img_height + 8, top: y, left: x}}>
+        <div
+            id={id}
+            className={`absolute rounded-md bg-tertiary p-1 ${className}`}
+            style={{ width: img_width + 8, height: img_height + 8, top: y, left: x }}
+        >
             <Link href={`/details/${o_id}`}>
-                <div className={`p-0`} style={{width: img_width}}>
+                <div className={`p-0`} style={{ width: img_width }}>
                     <div className={`relative`}>
-                        <Image 
-                            className={`p-0`} 
-                            src={`${PROJECT_CONSTANTS.AWS_BUCKET_URL}${image_path}`} 
-                            width={img_width} 
-                            height={img_height} 
-                            style={{width: img_width, height: img_height}} 
+                        <Image
+                            className={`p-0`}
+                            src={`${PROJECT_CONSTANTS.AWS_BUCKET_URL}${image_path}`}
+                            width={img_width}
+                            height={img_height}
+                            style={{ width: img_width, height: img_height }}
                             sizes="(min-width: 768px) 30vw, (max-width: 769px) 45vw"
-                            alt={title} 
+                            alt={title}
                         />
                     </div>
-                    { 
-                    (sold === true) ? (
+                    {sold === true ? (
                         <div className={`absolute !bottom-2.5 right-2.5`}>
-                            <Image className={`!top-auto !left-auto`} src="/redDot.png" alt="Piece Sold" width={30} height={30} priority={true}/> 
+                            <Image
+                                className={`!left-auto !top-auto`}
+                                src="/redDot.png"
+                                alt="Piece Sold"
+                                width={30}
+                                height={30}
+                                priority={true}
+                            />
                         </div>
-                    ) : ( 
-                        (USING_YELLOW_DOT && available === false) ? ( 
-                            <div className={`absolute !bottom-2.5 right-2.5`}>
-                                <Image className={`!top-auto !left-auto`} src="/yellowDot.png" alt="Piece Sold" layout='fixed' width={30} height={30}/> 
-                            </div>
-                        ) : (
-                            null
-                        ))
-                    }
+                    ) : USING_YELLOW_DOT && available === false ? (
+                        <div className={`absolute !bottom-2.5 right-2.5`}>
+                            <Image
+                                className={`!left-auto !top-auto`}
+                                src="/yellowDot.png"
+                                alt="Piece Sold"
+                                layout="fixed"
+                                width={30}
+                                height={30}
+                            />
+                        </div>
+                    ) : null}
                 </div>
             </Link>
         </div>
-    )
-}
+    );
+};
 
 export default Piece;
