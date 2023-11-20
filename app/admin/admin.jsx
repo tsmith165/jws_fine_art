9bb551eeb0f03472dc479703a7322e0a2d9b0400
Torrey Smith <tas.smity@yahoo.com>
2023-11-20 06:35:44 -0800

    updated sign_in / sign_up to tailwind

--- app/admin/admin.jsx
+++ app/admin/admin.jsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
@@ -35,12 +35,11 @@ function Admin(props) {
             return;
         }
         if (user.publicMetadata.role.toLowerCase() != 'admin') {
-            console.error('User is not an admin.  Redirecting to home page...')
+            console.error('User is not an admin.  Redirecting to home page...');
             router.push('/');
         }
 
         fetchData();
-
     }, [isLoaded, isSignedIn, user]);
 
     const renderAnalyticsData = () => {
@@ -100,38 +99,41 @@ function Admin(props) {
         }
     };
 
-    console.log(`isLoaded: ${isLoaded} isSignedIn: ${isSignedIn} user: ${user}`)
+    console.log(`isLoaded: ${isLoaded} isSignedIn: ${isSignedIn} user: ${user}`);
 
     if (!isLoaded) {
-        return <div>Loading...</div>
+        return <div>Loading...</div>;
     }
     if (!isSignedIn) {
-        console.log('User is not signed in.  Redirecting to signin page...')
-        return router.push('/signin')
+        console.log('User is not signed in.  Redirecting to signin page...');
+        return router.push('/signin');
     }
     if (!user || user.publicMetadata?.role?.toLowerCase() != 'admin') {
-        console.log(`Current user is not admin - Role: ${user.publicMetadata?.role?.toLowerCase() || 'none'}`)
-        return router.push('/signin')
+        console.log(`Current user is not admin - Role: ${user.publicMetadata?.role?.toLowerCase() || 'none'}`);
+        return router.push('/signin');
     }
 
     return (
         <div className={'flex justify-center'}>
-            <div className={'flex flex-col items-center w-full h-full p-5 overflow-x-hidden'}>
-                <div className={'w-full pb-5 border-2 border-secondary rounded-md'}>
-                    <div className={'w-full h-[40px] px-1 py-2 bg-secondary text-light font-lato text-lg rounded-tl-sm rounded-tr-sm'}>
+            <div className={'flex h-full w-full flex-col items-center overflow-x-hidden p-5'}>
+                <div className={'w-full rounded-md border-2 border-secondary pb-5'}>
+                    <div className={'h-[40px] w-full rounded-tl-sm rounded-tr-sm bg-secondary px-1 py-2 font-lato text-lg text-light'}>
                         Data Backup
                     </div>
-                    <div className={'w-full bg-primary rounded-bl-sm rounded-br-sm p-5'}>
-                        <button className={'px-2 py-5 bg-light text-dark font-lato border-none rounded-md cursor-pointer uppercase'} onClick={exportPiecesAsXLSX}>
+                    <div className={'w-full rounded-bl-sm rounded-br-sm bg-primary p-5'}>
+                        <button
+                            className={'cursor-pointer rounded-md border-none bg-light px-2 py-5 font-lato uppercase text-dark'}
+                            onClick={exportPiecesAsXLSX}
+                        >
                             Export Pieces as XLSX
                         </button>
                     </div>
                 </div>
-                <div className={'flex flex-col items-center w-full h-full p-5 overflow-x-hidden'}>
-                    <div className={'w-full h-[40px] px-1 py-2 bg-secondary text-light font-lato text-lg rounded-tl-sm rounded-tr-sm'}>Analytics Data</div>
-                    <div className={'w-full bg-primary rounded-bl-sm rounded-br-sm p-5'}>
-                        {renderAnalyticsData()}
+                <div className={'flex h-full w-full flex-col items-center overflow-x-hidden p-5'}>
+                    <div className={'h-[40px] w-full rounded-tl-sm rounded-tr-sm bg-secondary px-1 py-2 font-lato text-lg text-light'}>
+                        Analytics Data
                     </div>
+                    <div className={'w-full rounded-bl-sm rounded-br-sm bg-primary p-5'}>{renderAnalyticsData()}</div>
                 </div>
             </div>
         </div>
