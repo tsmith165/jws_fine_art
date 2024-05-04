c0994f0cc91d010f3b0dce763f2186e7db53bfc1
Torrey Smith <tas.smity@yahoo.com>
2024-05-03 17:16:58 -0700

    Fixing issue with checkout success page

--- app/checkout/actions.ts
+++ app/checkout/actions.ts
@@ -61,7 +61,7 @@ export async function runStripePurchase(data: FormData) {
             },
         ],
         mode: 'payment',
-        success_url: `${PROJECT_CONSTANTS.LOCAL_SITE_URL}/checkout/success`,
+        success_url: `${PROJECT_CONSTANTS.LOCAL_SITE_URL}/checkout/success/${piece.id}`,
         cancel_url: `${PROJECT_CONSTANTS.LOCAL_SITE_URL}/checkout/cancel/${piece.id}`,
         client_reference_id: piece.id.toString(),
     });
