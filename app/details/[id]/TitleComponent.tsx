afe65ee7ce052acf5fb50f319cb283637fa93b1d
Torrey Smith <tas.smity@yahoo.com>
2024-05-25 11:24:53 -0700

    updated edit page title bar to have up/down arrows

--- app/details/[id]/TitleComponent.tsx
+++ app/details/[id]/TitleComponent.tsx
@@ -15,7 +15,7 @@ const TitleComponent: React.FC<TitleComponentProps> = ({ title, next_id, last_id
             <div className="flex h-[58px] w-full flex-row items-center space-x-4">
                 <div className="flex h-[58px] flex-col space-y-1 p-1">
                     <Link href={`/details/${next_id}`} prefetch={true}>
-                        <IoIosArrowUp className="p- h-[22px] w-8 rounded-md bg-secondary_light fill-primary_dark hover:bg-primary hover:fill-primary_dark" />
+                        <IoIosArrowUp className="h-[22px] w-8 rounded-md bg-secondary_light fill-primary_dark hover:bg-primary hover:fill-primary_dark" />
                     </Link>
                     <Link href={`/details/${last_id}`} prefetch={true}>
                         <IoIosArrowDown className="h-[22px] w-8 rounded-md bg-secondary_light fill-primary_dark hover:bg-primary hover:fill-primary_dark" />
--- app/edit/[id]/Edit.tsx
+++ app/edit/[id]/Edit.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { IoIosArrowForward } from 'react-icons/io';
+import { IoIosArrowUp, IoIosArrowDown } from 'react-icons/io';
 import { MdPageview } from 'react-icons/md';
 
 import EditForm from './EditForm';
@@ -31,17 +31,19 @@ const Edit: React.FC<EditProps> = ({ piece, current_id, next_id, last_id }) => {
                 />
             </div>
             <div className="h-2/3 overflow-y-auto bg-secondary lg:h-full lg:w-1/3">
-                <div className="flex flex-row items-center space-x-4 bg-primary px-4 py-1">
-                    <Link href={`/edit/${last_id}`}>
-                        <IoIosArrowForward className="h-6 w-6 rotate-180 cursor-pointer text-secondary_dark hover:text-primary" />
-                    </Link>
-                    <h1 className="flex-grow text-2xl font-bold text-secondary_dark">{piece.title}</h1>
+                <div className="flex h-[58px] flex-row items-center space-x-4 bg-primary px-4">
+                    <div className="flex h-[58px] flex-col space-y-1 p-1 ">
+                        <Link href={`/edit/${next_id}`}>
+                            <IoIosArrowUp className="h-[22px] w-8 cursor-pointer rounded-lg bg-secondary fill-secondary_dark hover:bg-secondary_dark hover:fill-primary" />
+                        </Link>
+                        <Link href={`/edit/${last_id}`}>
+                            <IoIosArrowDown className="h-[22px] w-8 cursor-pointer rounded-lg bg-secondary fill-secondary_dark hover:bg-secondary_dark hover:fill-primary" />
+                        </Link>
+                    </div>
                     <Link href={`/details/${current_id}`}>
-                        <MdPageview className="h-6 w-6 cursor-pointer text-secondary_light hover:text-primary" />
+                        <MdPageview className="h-[48px] w-[48px] cursor-pointer rounded-lg bg-secondary fill-secondary_dark p-1 hover:bg-secondary_dark hover:fill-primary" />
                     </Link>
-                    <Link href={`/edit/${next_id}`}>
-                        <IoIosArrowForward className="h-6 w-6 cursor-pointer text-secondary_dark hover:text-primary" />
-                    </Link>
+                    <h1 className="m-0 text-2xl font-bold text-secondary_dark">{piece.title}</h1>
                 </div>
                 <EditForm current_piece={piece} />
                 <PieceOrderPanel current_piece={piece} />
