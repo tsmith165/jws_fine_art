b0c76b7a1505f0ac6045a92df56f3235ceaf4e11
Torrey Smith <tas.smity@yahoo.com>
2024-05-30 13:24:01 -0700

    Made some final updates to the filter menu coloring

--- app/gallery/FilterMenu.tsx
+++ app/gallery/FilterMenu.tsx
@@ -29,30 +29,32 @@ const FilterMenu: React.FC = () => {
         <div onMouseEnter={() => setFilterMenuOpen(true)} onMouseLeave={() => setFilterMenuOpen(false)}>
             <div
                 className={
-                    `absolute right-[120px] top-[40px] z-10 flex flex-row overflow-visible bg-secondary_dark p-[5px] text-secondary_light ` +
+                    `group absolute right-[125px] top-[40px] z-10 flex flex-row overflow-visible bg-secondary_dark p-[5px] text-secondary_light ` +
                     `hover:bg-secondary hover:text-secondary_dark ` +
-                    `${filterMenuOpen ? 'rounded-t-lg bg-secondary text-secondary_dark md:rounded-t-none md:rounded-tr-lg' : 'rounded-t-lg'}`
+                    `${filterMenuOpen ? 'rounded-t-lg bg-secondary_light text-secondary hover:bg-secondary_light md:rounded-t-none md:rounded-tr-lg' : 'rounded-t-lg'}`
                 }
                 onClick={(e) => {
                     e.preventDefault();
                     setFilterMenuOpen(!filterMenuOpen);
                 }}
             >
-                <FaSlidersH className="h-[30px] w-[30px]" />
+                <FaSlidersH
+                    className={`${filterMenuOpen ? 'fill-primary_dark' : 'fill-primary'} h-[30px] w-[30px] p-0.5 group-hover:fill-primary_dark`}
+                />
             </div>
             {filterMenuOpen === true && (
-                <div className="absolute right-0 top-[80px] flex h-[40px] w-fit flex-row rounded-bl-lg bg-secondary_light md:right-[160px] md:top-[40px] md:rounded-bl-none md:rounded-tl-lg md:bg-secondary_dark">
+                <div className="absolute right-0 top-[80px] flex h-[40px] w-fit flex-row rounded-bl-lg bg-secondary_light md:right-[165px] md:top-[40px] md:rounded-bl-none md:rounded-tl-lg md:bg-secondary_dark">
                     {THEME_FILTERS.map(([filter, Icon], i) => (
                         <div
                             key={i}
-                            className={`group p-[5px] ${filter === theme ? 'bg-secondary' : ' hover:bg-secondary'} ${i === 0 ? 'rounded-bl-lg md:rounded-bl-none md:rounded-tl-lg' : ''}`}
+                            className={`group p-[5px] ${filter === theme ? 'bg-secondary_light' : 'hover:bg-secondary_light'} ${i === 0 ? 'rounded-bl-lg md:rounded-bl-none md:rounded-tl-lg' : ''}`}
                             onClick={(e) => {
                                 e.preventDefault();
                                 setTheme(filter);
                             }}
                         >
                             <Icon
-                                className={`h-[30px] w-[30px] ${filter === theme ? 'fill-primary' : 'fill-primary_dark group-hover:fill-primary'}`}
+                                className={`h-[30px] w-[30px] ${filter === theme ? 'fill-primary_dark' : 'fill-primary group-hover:fill-primary_dark'}`}
                             />
                         </div>
                     ))}
