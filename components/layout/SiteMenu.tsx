1e2359cc1bb589c4b81738aaab2b7f98f2e85c1a
tsmith165 <torreysmith165@gmail.com>
2024-05-30 05:52:32 -0700

    Updated icons/colors for SiteMenu

--- components/layout/SiteMenu.tsx
+++ components/layout/SiteMenu.tsx
@@ -6,7 +6,7 @@ import { SignedIn, SignedOut, UserButton, useUser } from '@clerk/nextjs';
 
 import MenuOverlay from './menu/MenuOverlay';
 
-import { IoMenu } from 'react-icons/io5';
+import { IoIosMenu } from 'react-icons/io';
 import { MdAccountCircle } from 'react-icons/md';
 
 const SiteMenu = ({ currentPage }: { currentPage: string }) => {
@@ -23,7 +23,7 @@ const SiteMenu = ({ currentPage }: { currentPage: string }) => {
     return (
         <div className="flex w-fit flex-row">
             {/* Account Profile Button */}
-            <div className="hover:!fill-dark !h-[40px] !w-[40px] rounded-t-md bg-secondary_dark !fill-secondary_light p-1 hover:bg-secondary_light">
+            <div className="group !h-[40px] !w-[40px] rounded-t-md bg-secondary_dark p-1 hover:bg-secondary_light">
                 {isSignedIn ? (
                     <UserButton
                         appearance={{
@@ -36,19 +36,18 @@ const SiteMenu = ({ currentPage }: { currentPage: string }) => {
                     />
                 ) : (
                     <Link href="/signin">
-                        <MdAccountCircle className="h-full w-full" />
+                        <MdAccountCircle className="h-full w-full group-hover:fill-primary_dark" />
                     </Link>
                 )}
             </div>
 
             {/* Hamburger Button */}
             <div className={`p-0`} onMouseLeave={() => setMenuOpen(false)}>
-                <div className={`h-full w-full`} onClick={handleMenuToggle}>
-                    <IoMenu
-                        className={`!h-[40px] !w-[40px] bg-secondary_dark p-[2.5px] ${
-                            isMenuOpen ? 'bg-secondary_light !fill-primary_dark' : 'bg-secondary_dark !fill-secondary_light'
-                        } hover:!fill-dark rounded-t-md hover:bg-secondary_light`}
-                    />
+                <div
+                    className={`group h-full w-full rounded-t-md bg-secondary_dark p-[2.5px] hover:bg-secondary_light ${isMenuOpen ? 'bg-secondary' : 'bg-secondary_dark'}`}
+                    onClick={handleMenuToggle}
+                >
+                    <IoIosMenu className={`h-full w-full group-hover:fill-primary_dark`} />
                 </div>
 
                 {isMenuOpen && (
