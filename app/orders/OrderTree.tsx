cf5382728a1c4877eb17a32d433bf4fba289dabb
tsmith165 <torreysmith165@gmail.com>
2024-05-30 08:02:17 -0700

    Updated OrderTree styling for correct hover functionality

--- app/orders/OrderTree.tsx
+++ app/orders/OrderTree.tsx
@@ -6,15 +6,12 @@ interface OrderTreeProps {
 }
 
 const OrderTree: React.FC<OrderTreeProps> = (props) => {
-    const list_items = props.verified_list.map((verified_payment_data) => {
+    const list_items = props.verified_list.reverse().map((verified_payment_data) => {
         console.log(`Verified Payment Data (Next Line):`);
         console.log(verified_payment_data);
 
         return (
-            <div
-                className={'flex flex-row hover:bg-primary_dark hover:text-secondary_light'}
-                key={verified_payment_data.id || Math.random()}
-            >
+            <div className={'group flex flex-row hover:bg-secondary_dark'} key={verified_payment_data.id || Math.random()}>
                 <div className={'bg-primary_dark'}>
                     <img
                         className={'h-[150px] w-[150px] object-contain'}
@@ -23,44 +20,44 @@ const OrderTree: React.FC<OrderTreeProps> = (props) => {
                     />
                 </div>
                 <div className={'mb-auto mt-auto w-[30%] overflow-hidden overflow-ellipsis whitespace-nowrap p-2.5 pl-5 text-lg'}>
-                    <b className="text-dark">{verified_payment_data.piece_title}</b>
+                    <b className="text-secondary_dark group-hover:text-primary">{verified_payment_data.piece_title}</b>
                 </div>
                 <div className={'flex w-[70%] flex-col px-2.5 py-1'}>
                     <div className={'mb-auto mt-auto pr-4'}>
                         <div className={'inline-block !w-[100px]'}>
-                            <b className={'text-dark'}>Date: </b>
+                            <b className={'text-secondary_dark group-hover:text-primary'}>Date: </b>
                         </div>
-                        <b className="">{verified_payment_data.date}</b>
+                        <b className="text-secondary_dark group-hover:text-primary">{verified_payment_data.date}</b>
                     </div>
                     <div className={'mb-auto mt-auto pr-4'}>
                         <div className={'inline-block !w-[100px]'}>
-                            <b className={'text-dark'}>Name: </b>
+                            <b className={'text-secondary_dark group-hover:text-primary'}>Name: </b>
                         </div>
-                        <b className="">{verified_payment_data.full_name}</b>
+                        <b className="text-secondary_dark group-hover:text-primary">{verified_payment_data.full_name}</b>
                     </div>
                     <div className={'mb-auto mt-auto pr-4'}>
                         <div className={'inline-block !w-[100px]'}>
-                            <b className={'text-dark'}>Address: </b>
+                            <b className={'text-secondary_dark group-hover:text-primary'}>Address: </b>
                         </div>
-                        <b className="">{verified_payment_data.address}</b>
+                        <b className="text-secondary_dark group-hover:text-primary">{verified_payment_data.address}</b>
                     </div>
                     <div className={'mb-auto mt-auto pr-4'}>
                         <div className={'inline-block !w-[100px]'}>
-                            <b className={'text-dark'}>Phone: </b>
+                            <b className={'text-secondary_dark group-hover:text-primary'}>Phone: </b>
                         </div>
-                        <b className="">{verified_payment_data.phone}</b>
+                        <b className="text-secondary_dark group-hover:text-primary">{verified_payment_data.phone}</b>
                     </div>
                     <div className={'mb-auto mt-auto pr-4'}>
                         <div className={'inline-block !w-[100px]'}>
-                            <b className={'text-dark'}>Email: </b>
+                            <b className={'text-secondary_dark group-hover:text-primary'}>Email: </b>
                         </div>
-                        <b className="">{verified_payment_data.email}</b>
+                        <b className="text-secondary_dark group-hover:text-primary">{verified_payment_data.email}</b>
                     </div>
                     <div className={'mb-auto mt-auto pr-4'}>
                         <div className={'inline-block !w-[100px]'}>
-                            <b className={'text-dark'}>Price: </b>
+                            <b className={'text-secondary_dark group-hover:text-primary'}>Price: </b>
                         </div>
-                        <b className="">${verified_payment_data.price}</b>
+                        <b className="text-secondary_dark group-hover:text-primary">${verified_payment_data.price}</b>
                     </div>
                 </div>
             </div>
