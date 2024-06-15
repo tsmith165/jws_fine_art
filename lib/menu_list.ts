082b299b4b061249a153874f9ae5c3b20eb324c0
Torrey Smith <tas.smity@yahoo.com>
2024-06-15 14:20:02 -0700

    Updated menu to correctly use clerk

---
+++ lib/menu_list.ts
@@ -0,0 +1,31 @@
+type MenuItem = [string, string, boolean, string];
+
+export const SIGNED_OUT_MENU_LIST: MenuItem[] = [
+    ['biography', 'Biography', false, '/'],
+    ['gallery', 'Gallery', false, '/gallery'],
+    ['details', 'Piece Details', false, '/details'],
+    ['slideshow', 'Slideshow', false, '/slideshow'],
+    ['sign_in', 'Sign In', false, '/signin'],
+];
+
+export const SIGNED_IN_MENU_LIST: MenuItem[] = [
+    ['biography', 'Biography', false, '/'],
+    ['gallery', 'Gallery', false, '/gallery'],
+    ['details', 'Piece Details', false, '/details'],
+    ['slideshow', 'Slideshow', false, '/slideshow'],
+    ['profile', 'Profile', false, '/profile'],
+    ['sign_out', 'Sign Out', false, '/signout'],
+];
+
+export const ADMIN_MENU_LIST: MenuItem[] = [
+    ['biography', 'Biography', false, '/'],
+    ['gallery', 'Gallery', false, '/gallery'],
+    ['details', 'Details', false, '/details'],
+    ['slideshow', 'Slideshow', false, '/slideshow'],
+    ['profile', 'Profile', false, '/profile'],
+    ['edit_details', 'Edit Details', true, '/edit'],
+    ['management', 'Management', true, '/manage'],
+    ['orders', 'Orders', true, '/orders'],
+    ['admin', 'Admin', true, '/admin'],
+    ['sign_out', 'Sign Out', false, '/signout'],
+];
