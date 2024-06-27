2baea1fadef104a44660e51d89521986f808f3ea
Torrey Smith <tas.smity@yahoo.com>
2024-06-27 04:58:35 -0700

    Updated contact page

--- src/app/contact/contact.tsx
+++ src/app/contact/contact.tsx
@@ -1,27 +1,36 @@
+// File 1: /src/app/contact/contact.tsx
+
 import React from 'react';
 import Image from 'next/image';
-// import ContactForm from './contact_form';
+import Link from 'next/link';
 
 export default function Contact() {
     return (
-        <div className="flex h-full w-full items-center p-4 first-line:justify-center">
-            <div className="relative flex h-full max-h-full w-full flex-col overflow-y-scroll ">
+        <div className="flex h-full w-full items-center bg-stone-300 p-4">
+            <div className="flex h-full max-h-full w-full flex-col items-center justify-center space-y-2     overflow-y-scroll">
                 <Image
                     src="/bio/bio_pic_updated_small.jpg"
                     alt="Jill Weeks Smith"
                     width={300}
                     height={400}
                     quality={100}
-                    className="absolute left-0 right-0 m-auto h-[calc(45dvh-50px)] w-auto rounded-lg shadow-lg md:h-[calc(55dvh-50px)] lg:h-[calc(55dvh-50px)]"
+                    className="h-[calc(45dvh-50px)] w-auto rounded-lg shadow-lg md:h-[calc(55dvh-50px)] lg:h-[calc(55dvh-50px)]"
                 />
-                <div className="absolute top-[calc(40dvh-50px)] flex h-fit w-full flex-col items-center justify-center space-y-2 rounded-lg bg-secondary_light p-4 text-white shadow-lg md:top-[calc(50dvh-50px)] lg:top-[calc(50dvh-50px)]">
-                    <h1 className="w-fit bg-gradient-to-r from-secondary_dark via-primary_dark to-secondary_dark bg-clip-text text-center text-2xl font-bold text-transparent">
-                        Get in Touch with Jill
-                    </h1>
-                    <div className="mt-4 ">
-                        <p className="text-md text-secondary_dark">jwsfineart@gmail.com</p>
+                <Link href="mailto:jwsfineart@gmail.com" className="pt-2 text-lg text-blue-500 hover:text-blue-700">
+                    jwsfineart@gmail.com
+                </Link>
+                <Link href="https://www.instagram.com/jws_fineart/" target="_blank" rel="noreferrer" prefetch={false}>
+                    <div className="group flex flex-row items-center justify-center space-x-2 rounded-lg p-2 hover:bg-stone-500">
+                        <Image
+                            className={'h-[30px] w-[30px] rounded-t-md'}
+                            src="/icon/instagram_icon_50.png"
+                            alt="Instagram Link"
+                            width={40}
+                            height={40}
+                        />
+                        <span className="text-md text-center text-lg text-neutral-900 group-hover:text-white"></span>
                     </div>
-                </div>
+                </Link>
             </div>
         </div>
     );
