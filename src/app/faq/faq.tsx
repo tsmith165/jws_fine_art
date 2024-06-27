23f2ea25c109d5a0650d70126c5de71b23baeda7
Torrey Smith <tas.smity@yahoo.com>
2024-06-27 07:16:22 -0700

    Added Events/FAQ placeholders

--- src/app/faq/faq.tsx
+++ src/app/faq/faq.tsx
@@ -4,7 +4,7 @@ export default function FAQ() {
     return (
         <div className="flex h-full w-full flex-col items-center justify-center space-y-4 bg-stone-300 p-4">
             <h1 className="bg-gradient-to-r from-primary via-primary_dark to-primary bg-clip-text text-center text-4xl font-bold text-transparent">
-                Events
+                FAQ
             </h1>
             <p className="text-center text-lg text-neutral-900">Coming Soon!</p>
         </div>
