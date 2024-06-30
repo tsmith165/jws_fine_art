35c211dfbf4aa8ef15e1cbe2c0fe3617562c0379
Torrey Smith <tas.smity@yahoo.com>
2024-06-30 08:44:12 -0700

    Updated autocomplete input for mobile

--- src/components/inputs/InputAutoComplete.tsx
+++ src/components/inputs/InputAutoComplete.tsx
@@ -20,7 +20,7 @@ const InputAutoComplete: React.FC<InputAutoCompleteProps> = ({ name, value, onCh
                     });
 
                     return (
-                        <div className="m-0 w-full p-0">
+                        <div className="m-0 w-full max-w-full p-0">
                             <div className="flex w-full">
                                 <div className="flex h-8 min-w-28 max-w-28 items-center justify-center rounded-l-md bg-secondary_dark px-2.5">
                                     <b className="text-primary">Address</b>
@@ -38,7 +38,7 @@ const InputAutoComplete: React.FC<InputAutoCompleteProps> = ({ name, value, onCh
                                     );
                                     return (
                                         <div
-                                            className="h-8 overflow-hidden overflow-ellipsis whitespace-nowrap bg-primary text-secondary_dark last:rounded-b-md hover:bg-secondary_dark hover:text-primary"
+                                            className="h-8 overflow-hidden overflow-ellipsis whitespace-nowrap bg-primary px-2.5 text-secondary_dark last:rounded-b-md hover:bg-secondary_dark hover:text-primary"
                                             {...suggestionItemProps}
                                         >
                                             {suggestion.description}
