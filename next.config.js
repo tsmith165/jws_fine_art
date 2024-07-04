aff188a7c3edb9dd0a527272d3ff8165e5fa58a6
Torrey Smith <tas.smity@yahoo.com>
2024-07-04 10:31:37 -0700

    Updated to clerk 5.2.1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-import constants from './src/lib/constants.js';
-
-const { AWS_BUCKET_URL } = constants;
-
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
@@ -11,11 +7,6 @@ const nextConfig = {
     },
     images: {
         remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: AWS_BUCKET_URL.replace('https://', ''),
-                pathname: '**',
-            },
             {
                 protocol: 'https',
                 hostname: 'utfs.io',
