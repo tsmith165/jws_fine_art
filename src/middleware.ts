a8b423806872d3c6085fec6e21d684d5694a79db
Torrey Smith <tas.smity@yahoo.com>
2024-07-05 22:14:17 -0700

    Updated middleware for admin org

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,33 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
+import { isClerkUserIdAdmin } from '@/utils/auth/ClerkUtils';
 
-const isPrivateRoute = createRouteMatcher(['/admin(.*)']);
+const isPrivateRoute = createRouteMatcher(['/admin/inventory', '/admin/tools', '/admin/manage']);
 
 export default clerkMiddleware(async (auth, req) => {
     const route_is_private = isPrivateRoute(req);
     console.log('Current route: ' + req.url);
     console.log('Route is private? ' + route_is_private);
 
-    if (route_is_private) {
-        console.log('Route is private.  Protecting with ADMIN role.');
-        try {
-            auth().protect({ role: 'org:ADMIN' });
-        } catch (error) {
-            console.error('Error protecting route with ADMIN role:', error);
-            return NextResponse.redirect(new URL('/signin', req.url));
+    if (route_is_private === true) {
+        console.log('Route is private.  Protecting with admin role.');
+        console.log('Current auth object: ' + JSON.stringify(auth()));
+
+        const userId = auth().userId;
+        console.log('User ID: ' + userId);
+
+        if (!userId) {
+            console.log('User ID is null. Redirecting to home page.');
+            return NextResponse.redirect(new URL('/', req.url));
         }
-    } else {
-        return NextResponse.next();
+
+        const hasAdminRole = await isClerkUserIdAdmin(userId);
+        console.log('User hasAdminRole: ' + hasAdminRole);
+        if (!hasAdminRole) {
+            console.log('User does not have admin role. Redirecting to home page.');
+            return NextResponse.redirect(new URL('/', req.url));
+        }
+        console.log('User has admin role. Continuing...');
     }
     return NextResponse.next();
 });
--- src/utils/auth/ClerkUtils.ts
+++ src/utils/auth/ClerkUtils.ts
@@ -1,9 +1,7 @@
-import { User } from '@clerk/nextjs/dist/types/server';
-
-export async function captureClerkUserOrganizationMemberships(user: User) {
-    console.log("Capturing current user's organization memberships...");
+async function captureClerkUserOrganizationMemberships(userId: string) {
+    console.log("Capturing user's organization memberships...");
     try {
-        const response = await fetch(`https://api.clerk.com/v1/users/${user.id}/organization_memberships?limit=100`, {
+        const response = await fetch(`https://api.clerk.com/v1/users/${userId}/organization_memberships?limit=100`, {
             headers: {
                 Authorization: `Bearer ${process.env.CLERK_SECRET_KEY}`,
                 'Content-Type': 'application/json',
@@ -16,20 +14,24 @@ export async function captureClerkUserOrganizationMemberships(user: User) {
 
         const memberships = await response.json();
 
-        console.log('MenuOverlay: User Memberships:', memberships.data);
+        console.log('User Memberships:', memberships.data);
         return memberships;
     } catch (error) {
         console.error('Error fetching organization memberships:', error);
+        throw error;
     }
 }
 
-export async function isClerkUserAdmin(user: User) {
+export async function isClerkUserIdAdmin(userId: string) {
     let isAdmin = false;
-    if (user) {
-        const user_organization_memberships = await captureClerkUserOrganizationMemberships(user);
+    if (userId) {
+        const user_organization_memberships = await captureClerkUserOrganizationMemberships(userId);
         isAdmin = user_organization_memberships.data.some(
-            (membership: any) => membership.organization.name === 'ADMIN' && membership.role === 'admin',
+            (membership: any) =>
+                membership.organization.name === 'ADMIN' &&
+                (membership.role === 'org:admin' || membership.permissions.includes('org:sys_domains:manage')),
         );
     }
+    console.log('Is user admin:', isAdmin);
     return isAdmin;
 }
