d6c022d49befa2bc644cda09366cd609038a4d0e
Torrey Smith <tas.smity@yahoo.com>
2024-07-13 04:41:33 -0700

    Added memo to Homepage.tsx

--- src/app/Homepage.tsx
+++ src/app/Homepage.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import { motion, AnimatePresence } from 'framer-motion';
 import Link from 'next/link';
+import Head from 'next/head';
 
 interface HomepageData {
     id: number;
@@ -21,25 +22,32 @@ interface HomepageProps {
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const Homepage: React.FC<HomepageProps> = ({ homepageData }) => {
+    console.log('Rendering Homepage...');
     const [isLogoVisible, setLogoVisible] = useState(true);
     const [isImageVisible, setImageVisible] = useState(false);
-    const currentImageIndexRef = useRef(0);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const imageRefs = useRef<(HTMLDivElement | null)[]>([]);
 
     useEffect(() => {
-        const interval = setInterval(async () => {
-            setImageVisible(false);
-            await delay(1500);
-            currentImageIndexRef.current = (currentImageIndexRef.current + 1) % homepageData.length;
-            setImageVisible(true);
-        }, 5500);
+        const rotateImages = async () => {
+            while (true) {
+                await delay(5500); // Wait before starting transition
+                setImageVisible(false);
+                await delay(1500); // Wait for fade out
+                setCurrentImageIndex((prevIndex) => (prevIndex + 1) % homepageData.length);
+                await delay(100); // Small delay before fade in
+                setImageVisible(true);
+            }
+        };
 
-        setTimeout(async () => {
-            await delay(1000);
+        const startRotation = async () => {
+            await delay(250);
             setImageVisible(true);
-        }, 1000);
+            rotateImages();
+        };
 
-        return () => clearInterval(interval);
-    }, [homepageData]);
+        startRotation();
+    }, [homepageData.length]);
 
     const circularFadeVariants = {
         hidden: {
@@ -52,59 +60,74 @@ const Homepage: React.FC<HomepageProps> = ({ homepageData }) => {
     };
 
     return (
-        <div className="relative h-[calc(100dvh-50px)] w-full overflow-hidden bg-stone-900">
-            <AnimatePresence>
-                {isImageVisible && (
+        <>
+            <Head>
+                <link rel="preload" href={homepageData[0].image_path} as="image" />
+            </Head>
+            <div className="relative h-[calc(100dvh-50px)] w-full overflow-hidden bg-stone-900">
+                <AnimatePresence mode="wait">
+                    {isImageVisible && (
+                        <motion.div
+                            key={currentImageIndex}
+                            initial={{ opacity: 0, scale: 1 }}
+                            animate={{ opacity: 1, scale: 1.3 }}
+                            exit={{ opacity: 0, scale: 1 }}
+                            transition={{ duration: 3 }}
+                            className="absolute inset-0 h-full w-full"
+                        >
+                            <Image
+                                src={homepageData[currentImageIndex].image_path}
+                                width={homepageData[currentImageIndex].width}
+                                height={homepageData[currentImageIndex].height}
+                                className="absolute inset-0 h-full w-full object-cover"
+                                alt={homepageData[currentImageIndex].title}
+                                priority={currentImageIndex === 0}
+                                loading={currentImageIndex === 0 ? 'eager' : 'lazy'}
+                                placeholder="blur"
+                                blurDataURL={`data:image/svg+xml;base64,${btoa(
+                                    `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${homepageData[currentImageIndex].width} ${homepageData[currentImageIndex].height}">
+                                        <filter id="b" color-interpolation-filters="sRGB">
+                                            <feGaussianBlur stdDeviation="20" />
+                                        </filter>
+                                        <rect width="100%" height="100%" x="0" y="0" fill="#f0f0f0" filter="url(#b)" />
+                                    </svg>`,
+                                )}`}
+                            />
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+                <motion.div
+                    variants={circularFadeVariants}
+                    initial="hidden"
+                    animate={isImageVisible ? 'visible' : 'hidden'}
+                    transition={{ duration: 2 }}
+                    className="absolute inset-0 bg-neutral-900"
+                />
+                {isLogoVisible && (
                     <motion.div
-                        key={currentImageIndexRef.current}
-                        initial={{ opacity: 0, scale: 1 }}
-                        animate={{ opacity: 1, scale: 1.3 }}
-                        exit={{ opacity: 0, scale: 1 }}
-                        transition={{ duration: 3 }}
-                        className="absolute inset-0 h-full w-full"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 1 }}
+                        className="absolute inset-0 flex items-center justify-center"
                     >
-                        <Image
-                            src={homepageData[currentImageIndexRef.current].image_path}
-                            width={homepageData[currentImageIndexRef.current].width}
-                            height={homepageData[currentImageIndexRef.current].height}
-                            className="absolute inset-0 h-full w-full object-cover"
-                            alt={homepageData[currentImageIndexRef.current].title}
-                            priority
-                        />
+                        <div className="relative flex h-[250px] w-[250px] items-center justify-center rounded-full bg-stone-900 p-6 opacity-70 xxs:h-[300px] xxs:w-[300px] xs:h-[350px] xs:w-[350px]">
+                            <Image src="/logo/full_logo_small.png" alt="JWS Fine Art Logo" width={370} height={150} priority />
+                        </div>
                     </motion.div>
                 )}
-            </AnimatePresence>
-            <motion.div
-                variants={circularFadeVariants}
-                initial="hidden"
-                animate={isImageVisible ? 'visible' : 'hidden'}
-                transition={{ duration: 2 }}
-                className="absolute inset-0 bg-neutral-900"
-            ></motion.div>
-            {isLogoVisible && (
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 1 }}
-                    className="absolute inset-0 flex items-center justify-center"
-                >
-                    <div className="relative flex h-[250px] w-[250px] items-center justify-center rounded-full bg-stone-900 p-6 opacity-70 xxs:h-[300px] xxs:w-[300px] xs:h-[350px] xs:w-[350px]">
-                        <Image src="/logo/full_logo_small.png" alt="JWS Fine Art Logo" width={370} height={150} priority />
+                <div className="absolute bottom-8 left-0 w-full">
+                    <div className="flex h-fit w-full items-center justify-center space-x-2">
+                        <Link
+                            href="/gallery"
+                            className="rounded-full bg-primary px-6 py-2 text-secondary_dark opacity-55 transition-colors duration-300 hover:opacity-90 active:opacity-90"
+                        >
+                            Enter Gallery
+                        </Link>
                     </div>
-                </motion.div>
-            )}
-            <div className="absolute bottom-8 left-0 w-full">
-                <div className="flex h-fit w-full items-center justify-center space-x-2">
-                    <Link
-                        href="/gallery"
-                        className="rounded-full bg-primary px-6 py-2 text-secondary_dark opacity-55 transition-colors duration-300 hover:opacity-90 active:opacity-90"
-                    >
-                        Enter Gallery
-                    </Link>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
-export default Homepage;
+export default React.memo(Homepage);
