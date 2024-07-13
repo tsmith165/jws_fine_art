bbc1a3ee3d0a5245616dbb3006e59b5170fa5fff
Torrey Smith <tas.smity@yahoo.com>
2024-07-13 04:01:05 -0700

    Updated gallery to useMemo and useCallback

--- src/app/gallery/GalleryPage.tsx
+++ src/app/gallery/GalleryPage.tsx
@@ -3,22 +3,13 @@ import React from 'react';
 import Image from 'next/image';
 import dynamic from 'next/dynamic';
 
-const DynamicGallery = dynamic(() => import('./Gallery'), {
-    loading: () => (
-        <div className="inset-0 flex h-full w-full items-center justify-center">
-            <div className="relative flex h-[250px] w-[250px] items-center justify-center rounded-full bg-stone-900 p-6 opacity-70 xxs:h-[300px] xxs:w-[300px] xs:h-[350px] xs:w-[350px]">
-                <Image src="/logo/full_logo_small.png" alt="JWS Fine Art Logo" width={370} height={150} priority />
-            </div>
-        </div>
-    ),
-    ssr: false,
-});
+import Gallery from './Gallery';
 
 export default async function GalleryPage({ searchParams }: { searchParams: { piece?: string } }) {
     console.log(`Loading Gallery Page`);
     const piecesData = await fetchPieces();
 
-    return <DynamicGallery initialPieces={piecesData} />;
+    return <Gallery initialPieces={piecesData} />;
 }
 
 export const revalidate = 60;
